e6185d8b5707db018247353940fcdda363ad99cc
Fabrice H <fabrice@my-lovely-planet.com>
2022-12-16 20:04:11 +0400

    memory

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -123,6 +123,9 @@ class IndexController extends Controller
         $paysid = $spot->pays_id;
         $file = $request->file('img');
 
+
+
+
         if ($file == null) {
             // pas de nouvelle image
             $imagestatus = 0;
@@ -259,7 +262,7 @@ class IndexController extends Controller
         $spot = Spots::where('id', '=', $spotid)->first();
         // comptage des images total pour ce spot
         $spottotalcount = Pictures::where('spot_id', '=', $spotid)->where('actif', '=', 1)->where('user_id', '=', auth()->user()->id)->count();
-        $pictures = Pictures::where('user_id', '=', auth()->user()->id)->paginate(20);
+        $pictures = Pictures::where('user_id', '=', auth()->user()->id)->where('spot_id', '=', $spotid)->paginate(20);
         return view('frontend/addimagespot', compact('spot', 'spottotalcount', 'pictures'));
     }
 
