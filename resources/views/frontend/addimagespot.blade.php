06b40dbe24e6ab8f7d3903c6a88d3cd1755ca018
Fabrice H <fabrice@my-lovely-planet.com>
2022-12-17 19:12:32 +0400

    delete 2

--- resources/views/frontend/addimagespot.blade.php
+++ resources/views/frontend/addimagespot.blade.php
@@ -54,7 +54,7 @@
                  <div>
                   <img  src="{{ $picture->medium }}" class="dw-panel__content" alt="">
                  </div>
-                 <div><img class="addit" src="{{asset('frontend/assets/images/delete.png')}}" onClick=""></div>
+                 <div><a href="{}"><img class="addit" src="{{asset('frontend/assets/images/delete.png')}}"></a></div>
               </div>
              
       
