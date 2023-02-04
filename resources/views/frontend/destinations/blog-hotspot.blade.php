9d68d606dd2f3f50d3ae486f7d2e8602638afd64
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-02-04 15:54:18 +0400

    samedi 3

--- resources/views/frontend/destinations/blog-hotspot.blade.php
+++ resources/views/frontend/destinations/blog-hotspot.blade.php
@@ -22,7 +22,7 @@
     <h2 class="mt-4">Qu’est ce qu’un Point Chaud ?</h2>
     <div class="row mt-3">
      
-      <div class="col-6"><p>Un point chaud est le résultat d'une remontée de magma provenant d'une fusion partielle de matière dans le manteau inférieur. Il faut imaginer un panache de roche chaude qui remontent depuis les profondeurs de la Terre et et qui vient fondre à la base de la croute terrestre.  La chaleur monte par le processus de convection. 
+      <div class="col-6"><p>Un point chaud est une anomalie géologique. Bien que le mécanisme exact soit encore peu connu, c'est le résultat d'une remontée de magma provenant d'une fusion partielle de matière dans le manteau inférieur. Il faut imaginer un panache de roche chaude qui remontent depuis les profondeurs de la Terre et et qui vient fondre à la base de la croute terrestre.  La chaleur monte par le processus de convection. 
 </p><p>Lorsque la tête du panache atteint les couches supérieures, elle s'étend en forme de champignon atteignant un diamètre d'environ 500 à 1 000 kilomètres.
 Ensuite, le magma se fraye un chemin par des fractures pour créer des eruptions volcaniques.</p><p>Il a une durée de vie de plusieurs millions d'années et on peut le considérer comme fixe à l'échelle du globe.</p></div>
       <div class="col-6"><img class="card-img-top rounded" src="{{ asset('frontend/assets/images/blog/hotspot.jpeg')}}" alt=""></div>
@@ -41,29 +41,12 @@ Ensuite, le magma se fraye un chemin par des fractures pour créer des eruptions
             </div>
     </div>
     <h2 class="mt-4">Influence des points chaud sur la planète</h2>
-    <p>insi, un volcan situé au-dessus d'un point chaud n'est pas éternellement en éruption. Lorsque la plaque tectonique se déplace sur le point chaud (stationnaire), le volcan est déporté et de nouvelles éruptions se forment à sa place. Il en résulte des chaînes de volcans, comme les îles hawaïennes, Iles Marquises ou autres.</p>
-
+    <p>Un tel phénomène a un important impact sur la planète, il apporte le volcanisme au coeur des continents (comme à Yeloow Stone par exempe). Sur plusieurs millions d'années, il peut modeler les paysages en créant des chaines de volcans.</p>
+    <p>Un volcan situé au-dessus d'un point chaud n'est pas éternellement en éruption. Lorsque la plaque tectonique se déplace sur le point chaud (stationnaire), le volcan est déporté et de nouvelles éruptions se forment à sa place. Il en résulte des chaînes de volcans, comme les îles hawaïennes, Iles Marquises ou autres.</p>
+    <p>La théorie est encore discuté par les scientifiques et le concept est assez récent. En fonction des écrits que vous pourrez consulter certaines variantes sur la profondeur d’origine  du phénomène.
+La théorie dominante, élaborée par le géophysicien canadien J. Tuzo Wilson en 1963, stipule que les volcans de point chaud sont créés par des zones exceptionnellement chaudes fixées en profondeur sous le manteau terrestre. D’autres scientifiques pensent que leur origine est beaucoup plus profondes.</p>
 </div>
 
-Pourquoi sont ils important pour la construction des terres immergées ?
 
-
-
-
-
-Photo Carte Hot spots
-
- Si au cours des siècles on constate qu’un point chaud s’est déplacé par rapport à un point donné sur terre, c’est en fait  en raison du déplacement de la croute terrestre qui se déplace avec une plaque tectonique.
-A
-
-
-
-
-
-La théorie est encore discuté par les scientifiques et le concept est assez récent. En fonction des écrits que vous pourrez consulter certaines variantes sur la profondeur d’origine  du phénomène.
-
-La théorie dominante, élaborée par le géophysicien canadien J. Tuzo Wilson en 1963, stipule que les volcans de point chaud sont créés par des zones exceptionnellement chaudes fixées en profondeur sous le manteau terrestre.
-
-D’autres scientifiques pense que leur origine est beaucoup plus profondes.
 
 @endsection
