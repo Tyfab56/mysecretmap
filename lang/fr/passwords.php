d18e754c7ba89293ce453a940ce42fdb53dbba7c
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-04-12 17:57:22 +0400

    signup 4

---
+++ lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Votre mot depasse a étét mis à jour!',
+    'sent' => 'Vous allez recevoir un email pour mettre à jour le mot de passe',
+    'throttled' => 'Merci de patienter un peu, puis re-eesayez',
+    'token' => 'La demande de mise à jour a expirée',
+    'user' => "Personne avec cet email, désolé",
+
+];
