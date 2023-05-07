f7cbbdbc63a4b174d7cf2a963f875a41c3ad6f84
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-05-07 21:28:44 +0400

    dz19

--- resources/views/frontend/addimagespot.blade.php
+++ resources/views/frontend/addimagespot.blade.php
@@ -1,6 +1,5 @@
 @extends('frontend.main_master')
 @section('css')
-<link rel="stylesheet" href="{{ asset('frontend/assets/css/driveway.css')}}" />
 <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
 <link rel="stylesheet" href="https://unpkg.com/flexmasonry/dist/flexmasonry.css">
 
@@ -58,7 +57,29 @@ function delPicture (id)
             });
             }
   
-            FlexMasonry.init('.grid');
+            FlexMasonry.init('.grid',{
+    /*
+     * If `responsive` is `true`, `breakpointCols` will be used to determine
+     * how many columns a grid should have at a given responsive breakpoint.
+     */
+    responsive: true,
+    /*
+     * A list of how many columns should be shown at different responsive
+     * breakpoints, defined by media queries.
+     */
+    breakpointCols: {
+        'min-width: 1500px': 6,
+        'min-width: 1200px': 5,
+        'min-width: 992px': 4,
+        'min-width: 768px': 3,
+        'min-width: 576px': 2,
+    },
+    /*
+     * If `responsive` is `false`, this number of columns will always be shown,
+     * no matter the width of the screen.
+     */
+    numCols: 4,
+});
 @endsection
 @section('content')
 @auth
