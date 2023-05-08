6eb8681309892eb2141366dae69727cfcc025a6e
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-05-08 19:58:40 +0400

    pic2

--- resources/views/frontend/pictures.blade.php
+++ resources/views/frontend/pictures.blade.php
@@ -7,12 +7,32 @@
       
 <div class="flexbin flexbin-margin">         
               @foreach($pictures as $photo)
-              <a href="product/1.html"><img src="{{ $photo->medium }}" /></a>
+              <div class="smartbin-grid-item">
+                   <a href="product/1.html"><img src="{{ $photo->medium }}" /></a>
+              </div>
               @endforeach
           </div>
 
 
+<style>
+       .smartbin-grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: stretch;
+}
 
+.smartbin-grid-item {
+  margin-bottom: 16px;
+  width: calc(33.33% - 10px);
+}
+
+.smartbin-grid-item img {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+</style>
 @endsection
 @section('scripts')
 
