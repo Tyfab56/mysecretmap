da1ff47a6d17b4c7a7184096a35ecf313535c3f0
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-08-18 13:55:27 +0400

    shop

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\ShopifyWebhookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/shopify-webhook', 'ShopifyWebhookController@handleWebhook');
+Route::post('/shopify-webhook', [ShopifyWebhookController::class, 'handleWebhook']);
+
 
