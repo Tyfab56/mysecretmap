cf14456de08764857c2a9787e1d139df69e2f4e4
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-08-20 16:40:52 +0400

    audio

--- resources/views/frontend/audioguides.blade.php
+++ resources/views/frontend/audioguides.blade.php
@@ -5,11 +5,47 @@
 		<div class="col-md-12">
 			<div class="page-header">
 				<h1>
-                     Nous developpons des Applis & Audioguides Innovants
+                     Nous developpons des Applis & Audioguides touristiques
 				</h1>
 			</div>
 		</div>
 	</div>
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h2>
+            Un atout touristique
+			</h2>
+			<p>
+            Amélioration de l'expérience des visiteurs : Les audioguides permettent aux visiteurs de découvrir un lieu de manière interactive et immersive. Cela peut rendre leur expérience plus engageante, informative et divertissante, ce qui peut se traduire par des avis positifs et des retours favorables.
+
+Transmission d'informations précises : Les audioguides garantissent que les informations fournies aux visiteurs sont uniformes, précises et bien structurées. Cela évite les erreurs de communication et assure que les faits et les histoires sont transmis de manière cohérente.
+
+Attractivité pour les visiteurs internationaux : Les audioguides multilingues sont un atout pour les visiteurs internationaux qui ne parlent pas la langue locale. Cela élargit la portée de l'office du tourisme en attirant un public plus diversifié.
+
+Enrichissement culturel : Les audioguides peuvent raconter des histoires, expliquer les contextes historiques et culturels, et fournir des informations approfondies sur les sites et les monuments. Cela permet aux visiteurs de mieux comprendre et d'apprécier ce qu'ils voient.
+
+Flexibilité dans l'exploration : Les audioguides interactifs permettent aux visiteurs de choisir leur propre itinéraire et de passer plus de temps sur les éléments qui les intéressent le plus. Cela peut rendre l'expérience plus personnalisée.
+
+Engagement des visiteurs : Les audioguides interactifs et immersifs encouragent les visiteurs à interagir avec leur environnement. Cela peut rendre la visite plus mémorable et encourage les visiteurs à partager leur expérience avec d'autres.
+
+Nouveaux revenus : La location ou la vente d'audioguides peut être une source de revenus supplémentaire pour les offices du tourisme. Cela peut aider à financer d'autres initiatives touristiques et à améliorer les installations pour les visiteurs.
+
+Différenciation concurrentielle : Proposer des audioguides de haute qualité peut aider un office du tourisme à se démarquer de la concurrence en offrant une expérience unique et moderne.
+
+Accessibilité : Les audioguides adaptés aux personnes à besoins spécifiques, tels que les malvoyants ou les malentendants, rendent les sites touristiques plus accessibles à un public diversifié.
+
+En somme, les audioguides peuvent ajouter une valeur significative à l'offre d'un office du tourisme en améliorant l'expérience des visiteurs, en fournissant des informations enrichissantes et en répondant aux besoins variés des visiteurs.
+
+
+
+
+
+	</p>
+			
+		</div>
+	</div>
+</div>
 	<div class="row">
 		<div class="col-md-3">
 			
