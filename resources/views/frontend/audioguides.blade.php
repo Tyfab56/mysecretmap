38d3cb2b3e51a72af7a21d2bfc5f6e8135d78b2b
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-08-20 16:37:25 +0400

    audio

--- resources/views/frontend/audioguides.blade.php
+++ resources/views/frontend/audioguides.blade.php
@@ -12,16 +12,9 @@
 	</div>
 	<div class="row">
 		<div class="col-md-3">
-			<lt class="lt--mac-os">
-				<lt class="lt-highlighter__wrapper">
-					<lt class="lt-highlighter__scroll-element">
-						<canvas class="lt-highlighter__canvas">
-						</canvas>
-					</lt>
-				</lt>
-			</lt>
+			
 			<h3 class="text-center">
-				h3. Lorem ipsum dolor sit amet.
+				Offices du tourismes
 			</h3><img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded" />
 			<p>
 				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
@@ -29,7 +22,7 @@
 		</div>
 		<div class="col-md-3">
 			<h3>
-				h3. Lorem ipsum dolor sit amet.
+				Musées
 			</h3><img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" />
 			<p>
 				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
@@ -37,7 +30,7 @@
 		</div>
 		<div class="col-md-3">
 			<h3>
-				h3. Lorem ipsum dolor sit amet.
+				Collectivités locales
 			</h3><img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" />
 			<p>
 				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
@@ -45,7 +38,7 @@
 		</div>
 		<div class="col-md-3">
 			<h3>
-				h3. Lorem ipsum dolor sit amet.
+				Parcs et Jardins
 			</h3><img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" />
 			<lt class="lt--mac-os">
 				<lt class="lt-highlighter__wrapper">
