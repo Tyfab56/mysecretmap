d9daa679309cf1e525bca151878cebe7b7045187
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-08-21 09:29:47 +0400

    icon

--- resources/views/frontend/audioguides.blade.php
+++ resources/views/frontend/audioguides.blade.php
@@ -51,35 +51,35 @@
 	</div>
 </div>
 	<div class="row">
-		<div class="col-md-3">
+		<div class="col-md-3 center">
 			
-			<h3 class="text-center">
+			<h6 class="text-center">
 				Offices du tourismes
-			</h3><img loading="lazy" style="width: 80px;" src="{{  asset('frontend/assets/images/tourisme.png') }}" alt="My Secret map">
+			</h6><img loading="lazy" style="width: 80px;" src="{{  asset('frontend/assets/images/tourisme.png') }}" alt="My Secret map">
 			<p>
 				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
 			</p>
 		</div>
-		<div class="col-md-3">
-			<h3>
+		<div class="col-md-3 center">
+			<h6>
 				Musées
-			</h3><img loading="lazy" style="width: 80px;" src="{{  asset('frontend/assets/images/musee.png') }}" alt="My Secret map">
+			</h6><img loading="lazy" style="width: 80px;" src="{{  asset('frontend/assets/images/musee.png') }}" alt="My Secret map">
 			<p>
 				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
 			</p>
 		</div>
-		<div class="col-md-3">
-			<h3>
+		<div class="col-md-3 center">
+			<h6>
 				Collectivités locales
-			</h3><img loading="lazy" style="width: 80px;" src="{{  asset('frontend/assets/images/collectivite.png') }}" alt="My Secret map">
+			</h6><img loading="lazy" style="width: 80px;" src="{{  asset('frontend/assets/images/collectivite.png') }}" alt="My Secret map">
 			<p>
 				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
 			</p>
 		</div>
-		<div class="col-md-3">
-			<h3>
+		<div class="col-md-3 center">
+			<h6>
 				Parcs et Jardins
-			</h3><img loading="lazy" style="width: 80px;" src="{{  asset('frontend/assets/images/jardin.png') }}" alt="My Secret map">
+			</h6><img loading="lazy" style="width: 80px;" src="{{  asset('frontend/assets/images/jardin.png') }}" alt="My Secret map">
 			<lt class="lt--mac-os">
 				<lt class="lt-highlighter__wrapper">
 					<lt class="lt-highlighter__scroll-element">
