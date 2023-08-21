ed8ad7c9125be301be67f5e71fa4cb0fc4315ef8
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-08-21 10:49:26 +0400

    icon

--- resources/views/frontend/audioguides.blade.php
+++ resources/views/frontend/audioguides.blade.php
@@ -71,8 +71,7 @@
 				Collectivités locales
 			</h6><img loading="lazy" style="width: 80px;" src="{{  asset('frontend/assets/images/collectivite.png') }}" alt="My Secret map">
 			<p>
-				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
-			</p>
+            Promotion du patrimoine culturel, histoire de la collectivité, personnalités locales, les coutumes, les traditions et les réalisations qui ont marqué la collectivité, les traditions, les événements importants, lien vers les informations touristiques, visites autoguidées, événements et Festivals, support aux expositions, retours utilisateurs par récolte de données, actualité de la collectivité, 	</p>
 		</div>
 		<div class="col-md-3 center">
 			<h6>
