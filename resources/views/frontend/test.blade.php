ec98c6fbecc8451871190674def3dadc3a364df4
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-13 13:01:25 +0400

    test1

--- resources/views/frontend/test.blade.php
+++ resources/views/frontend/test.blade.php
@@ -8,10 +8,17 @@
 <div class="gridOverflow go-masonry go-zoomFx go-actionIcon">
 
     @foreach ($pictures as $picture)
-    <a class="go_gridItem" href="someURL">
-    <img src="{{ $picture->medium }}" /> 
+    <a class="go_gridItem" href="{{ $picture->medium }}">
+    <img src="{{ $picture->small}}" /> 
     </a>
     @endforeach
   <div class="go_gridItem go_gridItem-centered" href="someURL"><p> centered content - typically some text </p> </div>
 </div>
+<style>
+  .gridOverflow {
+    max-width: 90%;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  </style>
 @endsection
