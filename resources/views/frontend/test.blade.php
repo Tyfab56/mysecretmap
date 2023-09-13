dc4853b8a21a2bdc5c2115f1eb6bf1c65e85e96c
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-13 14:47:05 +0400

    test3

--- resources/views/frontend/test.blade.php
+++ resources/views/frontend/test.blade.php
@@ -6,16 +6,22 @@
 
 @section('content')
 <div class="gridOverflow go-masonry">
-
     @foreach ($pictures as $picture)
-    <a class="go_gridItem">
-         <img src="{{ $picture->medium}}" /> 
-    </a>
+    <div class="go_gridItem">
+        <div class="image-container">
+            <img src="{{ $picture->medium }}" alt="Image principale">
+            <div class="avatar-overlay">
+                <img class="avatar" src="{{ $picture->user->profile_photo_path }}" alt="Avatar">
+            </div>
+        </div>
+    </div>
     @endforeach
-  <div class="go_gridItem go_gridItem-centered" href="someURL"><p> centered content - typically some text </p> </div>
- 
+    <div class="go_gridItem go_gridItem-centered" href="someURL">
+        <p>centered content - typically some text</p>
+    </div>
 </div>
-<div class="row">{{ $pictures->links() }}
+<div class="row">{{ $pictures->links() }}</div>
+
 <style>
   .gridOverflow {
     max-width: 90%;
@@ -23,5 +29,34 @@
     margin-bottom: 10px;
     --masonryItemHeight: 180px;
   }
+
+  /* Style pour l'avatar */
+.image-container {
+    position: relative;
+}
+
+.avatar-overlay {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    overflow: hidden;
+    border-radius: 50%; /* Rend l'avatar rond */
+    background-color: rgba(0, 0, 0, 0.8); /* Fond semi-transparent */
+    display: none; /* Masque initialement l'avatar */
+    bottom: 0; /* Positionne l'avatar en bas */
+    right: 0; /* Positionne l'avatar à droite */
+    z-index: 1; /* Assurez-vous que l'avatar est au-dessus de l'image principale */
+}
+
+.avatar {
+    width: 100%;
+    height: auto;
+}
+
+/* Afficher l'avatar au survol de l'image */
+.image-container:hover .avatar-overlay {
+    display: block;
+}
+
   </style>
 @endsection
