d420a4e1e83db1b38b6eb8445c85d75b9403a153
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-13 20:07:36 +0400

    index

--- resources/views/frontend/searchspot.blade.php
+++ resources/views/frontend/searchspot.blade.php
@@ -1,10 +1,23 @@
 @extends('frontend.main_master')
 @section('content') 
 @foreach ($spots as $spot)
-    <div>
-        <p>{{ $spot->name }}</p> <!-- ou tout autre champ du modèle Spot que vous souhaitez afficher -->
-        <!-- Pour accéder à la traduction : -->
-        <p>Description: {{ $spot->translate(app()->getLocale())->description }}</p>
+    <div class="d-flex align-items-center mb-3">
+        <!-- Image à gauche -->
+        <img src="{{ $spot->imgsquaresmall }}" alt="{{ $spot->title }}" class="mr-3" width="100" height="100">
+
+        <!-- Titre et description à droite de l'image -->
+        <div class="flex-grow-1">
+            <h4>{{ $spot->title }}</h4>
+            <p>{{ $spot->translate(app()->getLocale())->description }}</p>
+        </div>
+
+        <!-- Bouton encore plus à droite -->
+        <div>
+            <a href="{{ route('destination', ['country_code' => $spot->country_code, 'spot_id' => $spot->id]) }}" class="btn btn-primary">
+                Voir plus
+            </a>
+        </div>
     </div>
 @endforeach
+
 @endsection
