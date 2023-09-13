1e365a36788768b3e7fdae0b9d79a05a137f4ab8
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-13 18:29:47 +0400

    index

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -41,39 +41,47 @@
         <div class="body-inner">
       
           <div id="top-bar" class="top-bar">
-              <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 col-md-8">
-                      <ul class="top-info text-center text-md-left ">
-                           <li><i class="fas fa-map-marker-alt"></i> <p class="info-text"></p>
-                          </li>
-                      </ul>
-                      
-                    </div>
-                    <!--/ Top info end -->
-        
-                    <div class="col-lg-4 col-md-4 top-social text-center text-md-right">
-                      <ul class="list-unstyled">
-                          <li>
-                            <a title="Facebook" href="https://www.facebook.com/mysecretmap.fr/">
-                                <span class="social-icon"><i class="fab fa-facebook-f"></i></span>
-                            </a>
-                            <a title="Twitter" href="https://twitter.com/FabriceH_photo">
-                                <span class="social-icon"><i class="fab fa-twitter"></i></span>
-                            </a>
-                            <a title="Instagram" href="https://www.instagram.com/my_secret_map/">
-                                <span class="social-icon"><i class="fab fa-instagram"></i></span>
-                            </a>
-                            <a title="Linkdin" href="https://www.linkedin.com/in/fabrice-h-03bb1a84/">
-                                <span class="social-icon"><i class="fab fa-github"></i></span>
-                            </a>
-                          </li>
-                      </ul>
-                    </div>
-                    <!--/ Top social end -->
-                </div>
-                <!--/ Content row end -->
-              </div>
+          <div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-md-4">
+            <ul class="top-info text-center text-md-left ">
+                <li><i class="fas fa-map-marker-alt"></i> <p class="info-text"></p>
+                </li>
+            </ul>
+        </div>
+        <!--/ Top info end -->
+
+        <!-- Zone de recherche -->
+        <div class="col-lg-4 col-md-4 text-center">
+            <form action="/path-to-search-handler" method="get">
+                <input type="text" name="search" placeholder="Recherche..." class="search-input">
+                <button type="submit" class="btn btn-search"><i class="fas fa-search"></i></button>
+            </form>
+        </div>
+
+        <div class="col-lg-4 col-md-4 top-social text-center text-md-right">
+            <ul class="list-unstyled">
+                <li>
+                    <a title="Facebook" href="https://www.facebook.com/mysecretmap.fr/">
+                        <span class="social-icon"><i class="fab fa-facebook-f"></i></span>
+                    </a>
+                    <a title="Twitter" href="https://twitter.com/FabriceH_photo">
+                        <span class="social-icon"><i class="fab fa-twitter"></i></span>
+                    </a>
+                    <a title="Instagram" href="https://www.instagram.com/my_secret_map/">
+                        <span class="social-icon"><i class="fab fa-instagram"></i></span>
+                    </a>
+                    <a title="Linkdin" href="https://www.linkedin.com/in/fabrice-h-03bb1a84/">
+                        <span class="social-icon"><i class="fab fa-github"></i></span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!--/ Top social end -->
+    </div>
+    <!--/ Content row end -->
+</div>
+
               <!--/ Container end -->
           </div>
 <!--/ Topbar end -->
