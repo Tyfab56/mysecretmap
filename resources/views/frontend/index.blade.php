7b5daa0f1144ebfb96977ae266c5c50ef683beda
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-13 17:13:55 +0400

    index

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -169,7 +169,24 @@
 
       </div>
 
-      
+      <div class="gridOverflow go-masonry">
+
+    @foreach ($pictures as $picture)
+    <a href="/destination/{{$picture->spot->pays_id}}/{{$picture->spot->id}}"class="go_gridItem">
+
+         <img src="{{ $picture->medium}}" /> 
+         <span class="go_caption go_caption-full">
+                {{ $picture->spot->name}}
+                <img src="{{ $picture->user->avatar}}" class="avatar" alt="{{$picture->user->pseudo}}"/> 
+         </span>
+         
+            
+    </a>
+    @endforeach
+  <div class="go_gridItem go_gridItem-centered" href="someURL"><p> centered content - typically some text </p> </div>
+ 
+</div>
+<div class="row">{{ $pictures->links() }}
 
     </div>
   </section>
