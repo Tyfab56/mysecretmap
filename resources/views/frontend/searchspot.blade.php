55971651386364a71832d6b249648e9b5ac5fc44
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-13 20:38:24 +0400

    index

--- resources/views/frontend/searchspot.blade.php
+++ resources/views/frontend/searchspot.blade.php
@@ -1,29 +1,43 @@
 @extends('frontend.main_master')
 @section('content') 
-@foreach ($spots as $spot)
+
 <section id="ts-features" class="ts-features">
     <div class="container">
-      <div class="row">
-    <div class="d-flex align-items-center mb-3">
-        <!-- Image à gauche -->
-        <img src="{{ $spot->imgsquaresmall }}" alt="{{ $spot->title }}" class="mr-3" width="100" height="100">
+        <div class="row">
+            <!-- Contenu principal -->
+            <div class="col-lg-8">
+                @foreach ($spots as $spot)
+                    <div class="d-flex align-items-center mb-3">
+                        <!-- Image à gauche -->
+                        <img src="{{ $spot->imgsquaresmall }}" alt="{{ $spot->name }}" class="mr-3" width="100" height="100">
 
-        <!-- Titre et description à droite de l'image -->
-        <div class="flex-grow-1">
-            <h4>{{ $spot->name }}</h4>
-            <p>{{ $spot->translate(app()->getLocale())->description }}</p>
-        </div>
+                        <!-- Titre et description à droite de l'image -->
+                        <div class="flex-grow-1">
+                            <h4>{{ $spot->name }}</h4>
+                            <p>{{ $spot->translate(app()->getLocale())->description }}</p>
+                        </div>
 
-        <!-- Bouton encore plus à droite -->
-        <div>
-            <a href="destination/{{$spot->pays_id}}/{{$spot->id}}" class="btn btn-primary">
-                Voir plus
-            </a>
+                        <!-- Bouton encore plus à droite -->
+                        <div>
+                            <a href="destination/{{$spot->pays_id}}/{{$spot->id}}" class="btn btn-primary">
+                                Voir plus
+                            </a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+
+            <!-- Sidebar à droite -->
+            <div class="col-lg-4">
+                <div class="sidebar">
+                    <!-- Contenu de votre sidebar ici -->
+                    <!-- Par exemple, une liste de liens, un widget, etc. -->
+                </div>
+            </div>
         </div>
     </div>
-   </div>
-  </div>
 </section>
-@endforeach
+
+
 
 @endsection
