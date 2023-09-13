358184407b99d2ba17bb371239610649b2580e86
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-13 15:47:45 +0400

    test5

--- resources/views/frontend/test.blade.php
+++ resources/views/frontend/test.blade.php
@@ -13,6 +13,9 @@
          <span class="go_caption go_caption-full">
                 {{ $picture->spot->name}}
          </span>
+         <div class="overlay">
+                <img src="{{ $picture->user->avatar }}" alt="Small Overlay Image">
+            </div>
       
             
     </a>
@@ -32,10 +35,6 @@
     opacity: 0;
     transition: opacity 0.3s ease-in-out;
 }
-.go_gridItem .go_avatar{
-    opacity: 0;
-    transition: opacity 0.3s ease-in-out;
-}
 
 /* Affiche le texte au survol de l'élément .go_gridItem */
 .go_gridItem:hover .go_caption {
@@ -51,17 +50,29 @@
 
   }
 
-  .go_gridItem:hover .go_avatar {
-    opacity: 1;
-    background-color: rgba(0, 0, 0, 0.5);
-    bottom: 0;
-    left: 0;
-    color: white;
-    box-sizing: border-box;
-    padding: 0.15rem 1rem;
-    text-align: center;
+  .overlay {
     position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1; /* Ensures the overlay is on top of the main image */
+    opacity: 0; /* Initially hide the overlay */
+    transition: opacity 0.3s ease-in-out; /* Add a smooth transition effect */
+}
 
-  }
+.go_gridItem:hover .overlay {
+    opacity: 1; /* Show the overlay on hover */
+}
+
+.overlay img {
+    width: 50px; /* Adjust the size of the overlay image as needed */
+    height: auto;
+}
+
+
+
+
+
+
+
   </style>
 @endsection
