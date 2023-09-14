32a5258a114fcc8bd3335e65e42f5f112dab409d
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-14 11:39:18 +0400

    profil

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'email',
         'password',
         'profile_photo_path',
-        'avatar,'
+        'avatar',
+        'pseudo',
         
     ];
 
