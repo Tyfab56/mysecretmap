e3b1d2cab21ae9e683934349a9c72c806bfbde85
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-14 20:42:52 +0400

    trans

--- app/Models/WhoIAmTranslation.php
+++ app/Models/WhoIAmTranslation.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class WhoIAmTranslation extends Model
+class WhoiamTranslation extends Model
 {
     /**
      * The attributes that are mass assignable.
