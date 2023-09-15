37f0d43d3670efc1472c0b3d5f65438dc964567e
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-15 15:16:47 +0400

    whoiam

--- app/Models/UserTranslation.php
+++ app/Models/UserTranslation.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class UserTranslation extends Model
+  
 {
-    public $timestamps = false;  // Désactivez les timestamps si vous n'en avez pas dans cette table
 
+    public $timestamps = false;  // Désactivez les timestamps si vous n'en avez pas dans cette table
+    protected $table = 'user_translations';
     protected $fillable = ['titre', 'description'];  // Les champs traduisibles
 
     /**
@@ -15,5 +17,5 @@ class UserTranslation extends Model
      *
      * @var string
      */
-    protected $table = 'users_translation';
+    
 }
