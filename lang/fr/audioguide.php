3c690d115783cedad7725ca6965a98348df6f310
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-19 22:01:14 +0400

    charly

--- lang/fr/audioguide.php
+++ lang/fr/audioguide.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    "AudioTitreIslande" => "Audioguide Islande",
-    "AudioDesc"=> "Découvrez l'Islande en compagnie du photographe Fabrice H.\n\nJ'ai conçu cet audioguide pour Iphone, Android et windows, à destination des amateurs de photographies qui ont envie de découvrir cette fantastique destination. Il existe actuellement en Français et en anglais. D'autres langues sont en préparation.\n",
+    "AudioTitreIslande" => "Charly vous guide",
+    "AudioDesc"=> "Partir à l'aventure en Islande, c'est bien. Mais confier l'optimisation de votre voyage à Charly, notre assistant et audioguide, c'est encore mieux. Il maîtrise parfaitement le pays, dévoile des lieux méconnus, et vous narre anecdotes et légendes qui enrichiront votre séjour. Charly vous conseille également pour réussir vos photos et vous informe sur la réglementation des drones. Avec Charly, c'est un expert que vous avez dans votre téléphone.",
     "Lancement" => "OFFRE DE LANCEMENT : 9€ au lieu de 12€",
     "Subscribe" => "Je profite de la promotion maintenant",
     "AdvTitre1" =>"Gagnez du temps sur la préparation votre voyage",
