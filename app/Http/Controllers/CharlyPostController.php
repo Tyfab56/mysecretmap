c4d39237d72eb9e70660c1cb114308fa9e8493f4
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-20 17:52:20 +0400

    charly

--- app/Http/Controllers/CharlyPostController.php
+++ app/Http/Controllers/CharlyPostController.php
@@ -11,7 +11,7 @@ class CharlyPostController extends Controller
     public function index($pays_id)
     {
         $posts = CharlyPost::where('pays_id', $pays_id)
-                        ->orderBy('rang', 'desc') // ou 'created_at' ou autre colonne selon votre choix
+                        ->orderBy('rank', 'desc') // ou 'created_at' ou autre colonne selon votre choix
                         ->paginate(10);
     
         return view('frontend.charlypost', compact('posts'));
