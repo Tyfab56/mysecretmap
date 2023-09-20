0d658ab0f398da39067a877f7b3e89ef8dcb236a
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-20 22:19:09 +0400

    audioguide

--- resources/views/admin/addspot.blade.php
+++ resources/views/admin/addspot.blade.php
@@ -20,7 +20,7 @@
                         <div class="col-12">
                             @if (Session::get('typeaction') == 'edit')
 
-                            <h3>Modification du spot</h3>
+                            <a href="/destination/{($spot->pays_id)}}/{($spot->id)}}"><h3>Modification du spot</h3></a>
                             
 
                             @else
@@ -28,6 +28,7 @@
                             <h3>Création du spot</h3>
 
                             @endif
+
                             @if (session('message'))
                                 <div class="alert alert-success">
                                     {{ session('message') }}
