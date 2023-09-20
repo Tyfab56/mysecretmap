d9d0e63bfc30a7428657cc851614e91ef184a2fc
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-20 23:03:09 +0400

    audioguide

--- resources/views/frontend/destination.blade.php
+++ resources/views/frontend/destination.blade.php
@@ -74,9 +74,9 @@
                 </div>
             </div>
               <div class="col-lg-3 bgbox"> 
-                @if {{$spot->audioguide}}
-                oH 
-                @endif
+              @if($spot->audioguide)
+                  oH
+              @endif
                 <livewire:show-map-spot />
             </div>
         </div>
