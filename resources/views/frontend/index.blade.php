cd952c16b8c2631a08777510a7af0e1fa128cd48
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-24 08:23:46 +0400

    liste

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -197,9 +197,8 @@
   <div class="go_gridItem go_gridItem-centered" href="someURL"><p> centered content - typically some text </p> </div>
  
 </div>
-<div class="row">{{ $pictures->links() }}
+{{ $pictures->links() }}
 
-    </div>
   </section>
 
   <section id="ts-features" class="ts-features">
