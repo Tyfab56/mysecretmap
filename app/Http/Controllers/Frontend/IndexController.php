b4de2c2efe253efc5afd78bba7bd6fdd89b54f72
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-26 16:09:45 +0400

    index

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -88,7 +88,8 @@ class IndexController extends Controller
                     ->orderBy('created_at', 'desc')
                     ->paginate(30);
 
-        $timelines = Timelines::orderBy('date', 'desc')->take(5)->get();           
+        $timelines = Timelines::orderBy('date', 'desc')->take(5)->get();   
+        dd($timelines);        
 
         return view('frontend/index', compact('lastPays', 'idpays', 'pays', 'payslist', 'payslng', 'payslat', 'payszoom', 'paysoffset', 'spot', 'lastspots', 'noscircuits','markerspays','pictures','timelines'));
     }
