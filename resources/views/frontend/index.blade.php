d617ceed6ad26d7b9615435bb86056f74eb521ca
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-10-02 14:17:10 +0400

    index

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -28,13 +28,13 @@
           <div class="col-lg-4 cold-md-12 bgbox p5 br5">
           <div class="row align-items-center">
                   <div class="col-md-8">
-                      <div class="jc-center ai-center d-flex justify-content-end">
+                      <div class="jc-center ai-center d-flex">
                           <i class="fas fa-headphones orange f1-4em"></i>
                           <h6 class="section-sub-title white ml-2 mr-2">{{__('index.Charly0')}}</h6>
                           <i class="fas fa-music orange f1-4em"></i>
                       </div>
                   </div>
-                  <div class="col-md-4 text-right">
+                  <div class="col-md-4">
                       <img src="{{ asset('frontend/assets/images/charly_80.png')}}" style="height:65px;" alt="charly" class="img-fluid">
                   </div>
               </div>
