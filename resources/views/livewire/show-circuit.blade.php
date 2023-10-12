a740ed21a03c0b69015523df2918d3e02e9d72e9
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-10-12 12:24:52 +0400

    timeonsite

--- resources/views/livewire/show-circuit.blade.php
+++ resources/views/livewire/show-circuit.blade.php
@@ -14,11 +14,11 @@
             <div class="list-info">
                  <div class="list-titre">
                  
-                 {{__('destination.Etape')} {{$point->rang}} :  <span class="orange">{{$point->spot()->first()->name}} </span>-  Trajet : {{ \Carbon\Carbon::parse($point->temps)->format('H:i') }} 
+                 {{__('destination.Etape')}} {{$point->rang}} :  <span class="orange">{{$point->spot()->first()->name}} </span>-  Trajet : {{ \Carbon\Carbon::parse($point->temps)->format('H:i') }} 
                     
-                    <p>{{__('destination.TempsSurSite')} : {{ \Carbon\Carbon::parse($point->timeonsite)->format('H:i') }}</p>
-                    <p>{{__('destination.TempsTotal')} : {{ \Carbon\Carbon::parse($point->tempscumul)->format('H:i') }}
-                    <p>{{__('destination.Distance')} :  {!! round(Helper::convertDistance($point->metres,'meter','kilometer'),1) !!}km ( Jour :  {!! round(Helper::convertDistance($point->metrescumul,'meter','kilometer'),1) !!} km)</p>
+                    <p>{{__('destination.TempsSurSite')}} : {{ \Carbon\Carbon::parse($point->timeonsite)->format('H:i') }}</p>
+                    <p>{{__('destination.TempsTotal')}} : {{ \Carbon\Carbon::parse($point->tempscumul)->format('H:i') }}
+                    <p>{{__('destination.Distance')}} :  {!! round(Helper::convertDistance($point->metres,'meter','kilometer'),1) !!}km ( Jour :  {!! round(Helper::convertDistance($point->metrescumul,'meter','kilometer'),1) !!} km)</p>
                 </div>
                 <div class="list-desc cornblue">{{$point->spot()->first()->description}}</div>
             </div>
