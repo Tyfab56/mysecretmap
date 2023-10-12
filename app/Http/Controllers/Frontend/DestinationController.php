4b40d6abc7e74999da01204d57114633ab4b0867
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-10-12 10:55:47 +0400

    timeonsite

--- app/Http/Controllers/Frontend/DestinationController.php
+++ app/Http/Controllers/Frontend/DestinationController.php
@@ -306,7 +306,7 @@ class DestinationController extends Controller
            // en faire le nouveau $spotencours
 
             // traitement : recherche du point le plus proche
-            $results = DB::select("SELECT id,spot_destination,temps,metres,geometry FROM distances WHERE spot_origine = ? AND temps = ( SELECT min(temps) FROM `distances` WHERE spot_origine = ? AND spot_destination IN ( SELECT spot_id FROM circuits_details WHERE circuit_id = ? AND rang > ? ) )", [$spotencours,$spotencours,$idcircuit,$i]);
+            $results = DB::select("SELECT id,spot_destination,temps,metres,geometry FROM distances WHERE spot_origine = ? AND temps = ( SELECT min(temps) FROM `distances` WHERE spot_origine = ? AND spot_destination IN ( SELECT spot_id FROM circuits_details WHERE circuit_id = ? AND rang > ? ) ) limit 1", [$spotencours,$spotencours,$idcircuit,$i]);
             // Donner à ce point l'indice i
             // faire de ce point le nouveau pointencours
             dd($results);
