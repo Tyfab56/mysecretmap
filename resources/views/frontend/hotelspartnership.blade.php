bda5d16650909445d6d9d281badcf6dc5a6f0593
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-10-19 17:01:29 +0400

    hotel

--- resources/views/frontend/hotelspartnership.blade.php
+++ resources/views/frontend/hotelspartnership.blade.php
@@ -11,14 +11,16 @@
             <h4 class="text-center mb-4">{!! __('audioguide.HotelSousTitre') !!}</h4>
 
             <div class="container">
-    <div>
-        <h2>Découvrez L'Islande Avec Charly</h2>
-        <p>Partez à l'aventure avec Charly, votre assistant de voyage audio, qui vous guide à travers 200 sites emblématiques d'Islande. Avec 5 heures d'audio à votre disposition, explorez non seulement les sites renommés, mais aussi des trésors cachés et moins visités. Grâce à Charly, chaque recoin de l'Islande s'anime pour une expérience de voyage authentique et inoubliable.</p>
-    </div>
-    <div>
-        <h2>Pourquoi s'associer à nous ?</h2>
-        <p>En devenant notre partenaire, vous offrez à vos clients une valeur ajoutée à leur séjour en Islande. Non seulement ils bénéficient d'un guide expert, mais ils peuvent aussi explorer des sites moins fréquentés, offrant ainsi une expérience unique à chaque voyageur. De plus, avec un QR code personnalisé pour votre établissement, vous pouvez suivre les ventes et bénéficier d'une commission pour chaque audioguide vendu.</p>
-    </div>
-</div>
+             <div style="display: flex; justify-content: space-between;">
+                    <div style="flex: 1; padding-right: 10px;">
+                        <h2>Découvrez L'Islande Avec Charly</h2>
+                        <p>Partez à l'aventure avec Charly, votre assistant de voyage audio, qui vous guide à travers 200 sites emblématiques d'Islande. Avec 5 heures d'audio à votre disposition, explorez non seulement les sites renommés, mais aussi des trésors cachés et moins visités. Grâce à Charly, chaque recoin de l'Islande s'anime pour une expérience de voyage authentique et inoubliable.</p>
+                    </div>
+                    <div style="flex: 1; padding-left: 10px;">
+                        <h2>Pourquoi s'associer à nous ?</h2>
+                        <p>En devenant notre partenaire, vous offrez à vos clients une valeur ajoutée à leur séjour en Islande. Non seulement ils bénéficient d'un guide expert, mais ils peuvent aussi explorer des sites moins fréquentés, offrant ainsi une expérience unique à chaque voyageur. De plus, avec un QR code personnalisé pour votre établissement, vous pouvez suivre les ventes et bénéficier d'une commission pour chaque audioguide vendu.</p>
+                    </div>
+             </div>
+            </div>
 </div>            
 @endsection
