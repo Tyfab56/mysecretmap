9f2cfd2a2b99e9137767857c14a8ecaf51599fc5
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-10-30 18:58:29 +0400

    video

--- app/Http/Livewire/AfficherCharly.php
+++ app/Http/Livewire/AfficherCharly.php
@@ -9,7 +9,7 @@ class AfficherCharly extends Component
 {
 
     public $charlyPost;
-    public $shouldRender = true;
+    public $shouldRender = false;
 
     protected $listeners = [
         'AfficheVideo'
@@ -20,16 +20,12 @@ class AfficherCharly extends Component
     {
         $locale = app()->getLocale();
 
-        // Vérifie si un post avec la locale spécifiée et l'idSpot existe.
+       
         $this->charlyPost = CharlyPost::whereHas('translations', function ($query) use ($locale) {
             $query->where('locale', $locale);
         })->where('spot_id', $idspot)->first();
 
-
-
-        // Si un post est trouvé, on doit rendre le composant, sinon non.
-        $this->shouldRender = true;
-        //$this->shouldRender = !is_null($this->charlyPost);
+        $this->shouldRender = !is_null($this->charlyPost);
     }
     public function render()
     {
@@ -37,6 +33,6 @@ class AfficherCharly extends Component
             return view('livewire.afficher-charly');
         }
 
-        return ''; // Ne rien rendre si le post n'existe pas
+        return ''; 
     }
 }
