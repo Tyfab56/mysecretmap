1348395f605c4074ae1e3d2579b12a88327fba98
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-11-11 16:21:15 +0400

    hotel

--- resources/views/admin/hotels.blade.php
+++ resources/views/admin/hotels.blade.php
@@ -11,28 +11,42 @@
 
     <a href="{{ route('admin.hotels.create') }}" class="btn btn-primary">Ajouter un nouvel hôtel</a>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Nom</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($hotels as $hotel)
-            <tr>
-                <td>{{ $hotel->name }}</td>
-                <td>
-                    {{-- Bouton ou lien pour supprimer un hôtel --}}
-                    <form action="{{ route('admin.hotels.destroy', $hotel->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Supprimer</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="container">
+    <div class="row">
+        <!-- Colonne pour la table -->
+        <div class="col-md-8">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($hotels as $hotel)
+                    <tr>
+                        <td>{{ $hotel->name }}</td>
+                        <td>
+                            {{-- Bouton ou lien pour supprimer un hôtel --}}
+                            <form action="{{ route('admin.hotels.destroy', $hotel->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Supprimer</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <!-- Colonne pour la carte -->
+        <div class="col-md-4">
+            <div id="map" style="height: 400px;">
+                {{-- Ici, vous pouvez intégrer votre carte --}}
+            </div>
+        </div>
+    </div>
 </div>
+
 @endsection
