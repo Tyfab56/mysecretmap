931bc3d4c5c72645e35b82b3293eef05b7df9c97
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-12-18 14:51:02 +0400

    windy

--- resources/views/livewire/windy-widget.blade.php
+++ resources/views/livewire/windy-widget.blade.php
@@ -3,8 +3,8 @@
         data-windywidget="forecast"
         data-thememode="white"
         data-tempunit="F"
-        data-lat="{{ $windylat }}"  
-        data-lon="{{ $windylng }}"  
+        data-lat= "{{ $windylat }}"  
+        data-lng= "{{ $windylng }}"  
         data-appid="widgets_d670075606">
     </div>
     <script async="true" data-cfasync="false" type="text/javascript" src="https://windy.app/widgets-code/forecast/windy_forecast_async.js?v1.4.14"></script>
