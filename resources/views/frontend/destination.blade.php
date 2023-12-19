40cbe43468c080ceb8e3f7c50dd854ddd03d91fe
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-12-19 11:52:14 +0400

    windy

--- resources/views/frontend/destination.blade.php
+++ resources/views/frontend/destination.blade.php
@@ -67,12 +67,12 @@
                           
 
                           <div class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">  
-                            <div class="col-lg-6 center pt-1 pb-1 pr-1 pl-1"><div id="mapzoom"></div></div>
+                            <div class="col-lg-12 center pt-1 pb-1 pr-1 pl-1"><div id="mapzoom"></div></div>
                            
                           </div> 
                           <div class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">  
                            
-                            <div class="col-lg-6 center pt-1 pb-1 pr-1 pl-1"><livewire:show-img-region /></div>
+                            <div class="col-lg-12 center pt-1 pb-1 pr-1 pl-1"><livewire:show-img-region /></div>
                           </div> 
                           
 
