2c033b4f0fbbb21e412f727bc56700bdc33ca27a
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-01-06 23:28:25 +0400

    hotel

--- resources/views/frontend/destination.blade.php
+++ resources/views/frontend/destination.blade.php
@@ -18,31 +18,26 @@
 @section('content')
 <section id="ts-features" class="ts-features">
     <div class="container">
-        <div class="row">
-            
-        </div>
+        
         <div class="row mt5 mb5">
-        <div class="col-lg-2 mb5 d-flex flex-column justify-content-center align-items-center">    
-    <h1><a href='{{ route($pays->route) }}'>{{$pays->pays}}</a></h1>                        
-    <a class="btn btn-primary f0-7m m5" href="{{ url('thewall') }}/{{ $idpays}}">{{__('destination.cherchespot')}}</a>
-</div>
+             <div class="col-lg-2 mb5 d-flex flex-column justify-content-center align-items-center">    
+                <h1><a href='{{ route($pays->route) }}'>{{$pays->pays}}</a></h1>                        
+                <a class="btn btn-primary f0-7m m5" href="{{ url('thewall') }}/{{ $idpays}}">{{__('destination.cherchespot')}}</a>
+            </div>
 
-
-    <!-- Modified Bandeau on the right -->
-    <div class="col-lg-10" style="display: flex; justify-content: center; align-items: center;">
-        <a href="https://www.villa-laurina.com" target="_blank">
-            <img src="{{ asset('frontend/assets/images/blog/pub1.jpg')}}" alt="Publicité" class="img-fluid">
-        </a>
-    </div>
-
-</div>
+            <div class="col-lg-10" style="display: flex; justify-content: center; align-items: center;">
+                <a href="https://www.villa-laurina.com" target="_blank">
+                    <img src="{{ asset('frontend/assets/images/blog/pub1.jpg')}}" alt="Publicité" class="img-fluid">
+                </a>
+            </div>
+        </div>
 
         <div class="row">        
                <div class="col-lg-9">
                      <div class="row">
                             <div class="row bgregbox w100">
                                 <div class="col-lg-12">
-                                <p>{{__('destination.alentours')}}</p>
+                                         <p>{{__('destination.alentours')}}</p>
                                   <div id="medias" class="mb5"></div>
                                </div>
                            </div>
@@ -50,59 +45,44 @@
                      <div class="row">
                              <div class="row bgregbox w100">
                                 <div class="col-lg-12">
-                                  <p>{{__('destination.clickmap')}}</p>
-                                        <div class="leaflet-map">  </div>    
+                                        <p>{{__('destination.clickmap')}}</p>
+                                        <div class="leaflet-map">
+                                        </div>    
+                                        <div class="row">
+                                                    <a name="mapPos" style="position:absolute; top:+100px;"></a>
+                                                    <div id="mapdest">
+                                                          <a href="https://www.maptiler.com" style="position:absolute;left:10px;bottom:10px;z-index:999;"><img src="https://api.maptiler.com/resources/logo.svg" alt="MapTiler logo"></a>
+                                                    </div>
+                                                    <div class="row">
+                                                          <div class="col-sm-12"><input type="checkbox" id="sunToggle" class="js-switch"><span class="p5 f0-8m" >{{__('destination.sunsetting')}}</span></div>
+                                                    </div>
+                                        </div>                                                         
+                                        <div id="sun" class="row min100 hidden">
                                         
-                                </div>
-                             
-                               </div> 
-                      <div class="row">
-
-                              <a name="mapPos" style="position:absolute; top:+100px;"></a>
-                              <div id="mapdest">
-                                  <a href="https://www.maptiler.com" style="position:absolute;left:10px;bottom:10px;z-index:999;"><img src="https://api.maptiler.com/resources/logo.svg" alt="MapTiler logo"></a>
+                                            <div class="col-sm-6"> 
+                                                  <div style="padding : 5px"><b>{{__('destination.choiceday')}} :</b> <span id="theday"></span></div>
+                                                  <div style="padding: 5px;"> <input type="range" class="form-range" min="1" max="365" oninput="changeRange(this.value)" onchange="changeRange(this.value)" id="dayofyear"></div>
+                                            </div>
+                                            <div class="col-sm-6"> 
+                                                  <div style="padding : 5px"><b>{{__('destination.choicehour')}} :</b> <span id="thehour"></span><span id="thedayhour"></span></div>
+                                                  <div style="padding: 5px;"> <input type="range" class="form-range" min="0" max="24" oninput="changeHour(this.value)" onchange="changeHour(this.value)" id="hourofday"></div>
+                                              </div> 
+                                              <p class="center w100"><span style="color:red"><b>{{__('destination.RedLine')}}</b></span> / <span style="color:orange"><b>{{__('destination.OrangeLine')}}</b></span></p><br/>
+                                        </div>              
+                                        <div class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">  
+                                            <div class="col-lg-12 pt-1 pb-1  pr-1 pl-1"><livewire:show-map-globale />
+                                          </div>
+                                        </div>                   
+                                        <div id ="wrap_video" class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">  
+                                            <div id="container_video" ></div>
+                                       </div>                          
+                                       <div class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">  
+                                      
+                                        <div class="col-lg-12 center pt-1 pb-1 pr-1 pl-1"><livewire:show-img-region /></div>
+                                       </div>                      
+                                 </div>
                               </div>
-                              <div class="row">
-                              <div class="col-sm-12"><input type="checkbox" id="sunToggle" class="js-switch"><span class="p5 f0-8m" >{{__('destination.sunsetting')}}</span></div>
-                              </div>
-                              
-                              <div id="sun" class="row min100 hidden">
-                                
-                                    <div class="col-sm-6"> 
-                                          <div style="padding : 5px"><b>{{__('destination.choiceday')}} :</b> <span id="theday"></span></div>
-                                          <div style="padding: 5px;"> <input type="range" class="form-range" min="1" max="365" oninput="changeRange(this.value)" onchange="changeRange(this.value)" id="dayofyear"></div>
-                                    </div>
-                                    <div class="col-sm-6"> 
-                                          <div style="padding : 5px"><b>{{__('destination.choicehour')}} :</b> <span id="thehour"></span><span id="thedayhour"></span></div>
-                                          <div style="padding: 5px;"> <input type="range" class="form-range" min="0" max="24" oninput="changeHour(this.value)" onchange="changeHour(this.value)" id="hourofday"></div>
-                                      </div> 
-                                      <p class="center w100"><span style="color:red"><b>{{__('destination.RedLine')}}</b></span> / <span style="color:orange"><b>{{__('destination.OrangeLine')}}</b></span></p><br/>
-                                </div>  
-                                
-                          </div>
-              
-                          <div class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">  
-                                <div class="col-lg-12 pt-1 pb-1  pr-1 pl-1"><livewire:show-map-globale /></div>
-                          </div> 
-                       
-                          <div id ="wrap_video" class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">  
-                                <div id="container_video" ></div>
-                          </div> 
-
-                          
-                          <div class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">  
-                           
-                            <div class="col-lg-12 center pt-1 pb-1 pr-1 pl-1"><livewire:show-img-region /></div>
-                          </div> 
-                        
-
-                          
-
-                                  
-                        
-                    </div>
-
-               </div>
+                       </div>      
                <div class="col-lg-3 bgbox">           
                         <livewire:show-map-spot />
                </div>
