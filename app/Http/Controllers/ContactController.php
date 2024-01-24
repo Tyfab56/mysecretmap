f5c708ebe1b215bfd0ae54a97b5eecbf2dc4bf55
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-01-24 16:55:07 +0400

    contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactFormConfirmation;
 
 class ContactController extends Controller
 {
