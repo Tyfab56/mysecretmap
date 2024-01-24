398984a554c00500b9026544da2d11b9cd88d754
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-01-24 13:25:54 +0400

    ventes

--- lang/fr/blog.php
+++ lang/fr/blog.php
@@ -11,5 +11,12 @@ return [
     "OtSpot3" => "Qui sommes nous ?",
     "OtSpot4" => "La société My Lovely Planet, basée à la Réunion, réalise <b>des outils numériques de promotion et d’assistance touristique</b>. Nous aidons les visiteurs à préparer et optimiser leur visite : lieux incontournables, lieux peu connus, spots pour photographes, information drone, histoires, légendes et anecdotes, conseils sanitaires et pratiques ...
     ",
+    "OtSpot5"=>"Nous combinons plusieurs supports, avec des densités d’informations différentes :",
+    "OtSpot6"=>"<b>Sur le Web</b>, nous avons un outil d’informations complet en plusieurs langues, avec de nombreux textes, des photos et vidéos, des cartes, circuit de randonnées et en bonus un simulateur de lumière pour les photographes. ",
+    "OtSpot7"=>"<b>Sur l'application audioguide 'Charly'</b>, chaque lieu bénéficie d'une présentation concise et géolocalisée. Ainsi, les utilisateurs peuvent écouter des explications en temps réel lorsqu'ils sont sur place. De nombreux voyageurs l'apprécient pour la découverte de sites en voiture, écoutant via l'autoradio et sélectionnant les spots qui retiennent le plus leur attention.
+    ",
+    "OtSpot8"=>"<b>Sur les réseaux sociaux</b>, nous sommes en mode “découverte”. Nous concevons des campagnes de communication avec un enchainement de Quiz, photos et vidéos d’explications en relation avec l’audioguide.",
+
+
     
 ];
--- resources/views/frontend/addotspot.blade.php
+++ resources/views/frontend/addotspot.blade.php
@@ -4,7 +4,12 @@
         <h1>{{ __('blog.OtSpot1') }}</h1>
         <p>{{ __('blog.OtSpot2') }}</p>
         <h4>{{ __('blog.OtSpot3') }}</h4>
-        <p>{{ __('blog.OtSpot4') }}</p>
+        <p>{!! __('blog.OtSpot4') !!}</p>
+        <p></p>
+        <p>{!! __('blog.OtSpot5') !!}</p>
+        <p>{!! __('blog.OtSpot6') !!}</p>
+        <p>{!! __('blog.OtSpot7') !!}</p>
+        <p>{!! __('blog.OtSpot8') !!}</p>
         <ul>
             <li><strong>Atteignez une Audience Engagée</strong>: Notre communauté est à la recherche de nouvelles découvertes.</li>
             <li><strong>Mettez en Lumière Votre Région</strong>: Valorisez les merveilles cachées de votre localité.</li>
