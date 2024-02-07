6102c9d10bd07bd66754b27e76695c8c540ad6b6
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-02-07 16:42:11 +0400

    hotels

--- resources/views/livewire/hotels-nearby.blade.php
+++ resources/views/livewire/hotels-nearby.blade.php
@@ -1,19 +1,27 @@
-<div>
-@if(!empty($hotels))
-    <div style="display: flex; flex-wrap: wrap; background-color: #f5f5f5; padding: 20px;">
-        @foreach ($hotels as $hotel)
-            <div style="flex: 1; min-width: 20%; max-width: 20%; box-shadow: 0 2px 4px rgba(0,0,0,0.1); margin: 10px; background-color: #ffffff;">
-                <a href="{{ $hotel->website_url }}" target="_blank" style="display: inline-block; width: 100%; color: inherit; text-decoration: none;">
-                    <img src="{{ $hotel->image_url }}" alt="Image de {{ $hotel->name }}" style="width: 100%; height: auto;">
-                    <div style="padding: 10px;">
-                        <h3 style="margin: 0; font-size: 16px;">{{ $hotel->name }}</h3>
-                        <p style="margin: 0; font-size: 14px;">{{ round($hotel->distance, 2) }} km</p>
-                    </div>
-                </a>
-            </div>
-        @endforeach
+<div wire:ignore x-data="{ swiper: null }"
+     x-init="swiper = new Swiper('.hotels-swiper', {
+                slidesPerView: 3,
+                spaceBetween: 30,
+                pagination: {
+                  el: '.swiper-pagination',
+                  clickable: true,
+                },
+                navigation: {
+                  nextEl: '.swiper-button-next',
+                  prevEl: '.swiper-button-prev',
+                },
+              })">
+    <div class="swiper hotels-swiper">
+        <div class="swiper-wrapper">
+            @foreach ($hotels as $hotel)
+                <div class="swiper-slide">
+                    <img src="{{ $hotel->image_url }}" alt="Image de {{ $hotel->name }}" style="width: 100%;">
+                    <div>{{ $hotel->name }}</div>
+                </div>
+            @endforeach
+        </div>
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
     </div>
-@endif
-
-
 </div>
