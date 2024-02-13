660ac1081f21ba008b5af0b126ac5c913ff12142
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-02-13 09:41:00 +0400

    affiliate

--- resources/views/frontend/affiliate.blade.php
+++ resources/views/frontend/affiliate.blade.php
@@ -1,4 +1,41 @@
 @extends('frontend.main_master')
 @section('content')
-Affiliate
+<div class="ud-main-content">
+    <!-- Introduction au Programme d'Affiliation -->
+    <div class="intro-affiliation">
+        <p>Rejoignez le programme d'affiliation de MySecretMap et aidez votre communauté à découvrir les plus belles destinations grâce à nos audioguides. Ensemble, explorons, apprenons et prospérons.</p>
+        <a href="https://mysecretmap.bixgrow.com" class="ud-btn ud-btn-large ud-btn-brand">Rejoignez-nous Maintenant !</a>
+       
+    </div>
+
+    <!-- Avantages de Devenir Partenaire Affilié -->
+    <div class="avantages-affiliation">
+        <ul>
+            <li>Accès à des milliers de cours dans une variété de sujets.</li>
+            <li>Promotion de contenu de qualité pour vos abonnés.</li>
+            <li>Générez des revenus grâce à des commissions compétitives.</li>
+        </ul>
+    </div>
+
+    <!-- Ressources pour les Affiliés -->
+    <div class="ressources-affiliation">
+        <h4>Ressources affiliées</h4>
+        <ul>
+            <li><a href="https://affiliatesupport.udemy.com/hc/en-us/articles/360049411833-What-are-the-requirements-to-join-the-Udemy-Affiliate-Program-">Comment s'inscrire</a></li>
+            <li><a href="https://affiliatesupport.udemy.com/hc/en-us/articles/360048655014-How-do-I-join-the-Udemy-Affiliate-Program-">Inscrivez-vous dès maintenant</a></li>
+        </ul>
+    </div>
+
+    <!-- FAQ du Programme d'Affiliation -->
+    <div class="faq-affiliation">
+        <h4>FAQ du programme d'affiliation</h4>
+        <ul>
+            <li><strong>Combien d'argent puis-je gagner ?</strong> Nous offrons des taux de commission compétitifs.</li>
+            <li><strong>Que dois-je promouvoir ?</strong> C'est vous qui décidez ! Vous choisissez les cours et les liens.</li>
+            <li><strong>Ai-je accès aux promotions et aux réductions spéciales ?</strong> Oui, en tant que partenaire affilié.</li>
+            <li><strong>Combien de temps faut-il pour commencer ?</strong> Inscrivez-vous aujourd'hui, et nous étudierons votre demande.</li>
+        </ul>
+    </div>
+</div>
+
 @endsection
