00c56a83f365cff9c8f9cc00efbfd7092fd05d21
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-02-14 15:31:57 +0400

    flag

--- config/languages.php
+++ config/languages.php
@@ -1,5 +1,15 @@
 <?php
+
+
 return [
-    'en' => 'English',
-    'fr' => 'Français'
+    'languages' => [
+        'en' => 'English',
+        'fr' => 'Français',
+        // autres langues
+    ],
+    'languages_flags' => [
+        'en' => 'us', // Code pays ISO 3166-1-alpha-2 pour les États-Unis
+        'fr' => 'fr', // Code pays pour la France
+        // autres correspondances de drapeaux
+    ],
 ];
