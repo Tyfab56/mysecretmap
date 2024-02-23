e83ceba1a94c59c0c23c1e85b5f1639543929485
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-02-23 16:31:05 +0400

    rando

--- resources/views/admin/randos/edit.blade.php
+++ resources/views/admin/randos/edit.blade.php
@@ -39,6 +39,27 @@
 </div>
 
 
+<div class="max-w-lg mx-auto my-10">
+  <div class="testimonial-card bg-white p-6 rounded-lg text-gray-800">
+    <div class="flex space-x-4">
+      <img class="w-24 h-24 rounded-full border-4 border-yellow-300" src="https://placehold.co/100x100.png" alt="Portrait of Aliya, a client, wearing sunglasses and a floral outfit">
+      <div class="flex flex-col justify-center">
+        <p class="font-semibold text-lg">Aliya</p>
+        <p class="text-yellow-500">Client</p>
+      </div>
+    </div>
+    <div class="mt-4">
+      <p class="text-gray-600">Lorem ipsum dolor sit amet consectetur adipiscing elit. Corporis consequuntur repellendus nemo suscipit explicabo veniam similique quaerat vitae! Alias reprehenderit aliquam temporibus porro iste corupti laboriosam nihil eos? Nemo ratione, prévoyante ! Suscipit, accusantium. Molestias, reiciendis, in nihil perferendis similique voluptas aliquam, nisi consectetur eum, atque nemo sint. Nostrum, répréhensible, quod.</p>
+    </div>
+  </div> 
+</div>
+
+<style>
+  .testimonial-card {
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1);
+  }
+</style>
+
 <script>
 $(document).ready(function() {
     // Fonction pour charger les traductions
