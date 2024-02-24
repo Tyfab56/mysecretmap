7bb028324449f2ddb4c74ae0d705abb6e4ac0f79
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-02-24 10:17:40 +0400

    rando

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -219,27 +219,22 @@
       </div>
 </section>
 <style>
-  body {
-    font-family: 'Inter', sans-serif;
-    background-color: #f3f4f6; /* bg-gray-100 */
-    margin: 0;
-    padding: 0;
-  }
+
   .testimonial-card {
     background-color: #ffffff; /* bg-white */
     padding: 1.5rem; /* p-6 */
     border-radius: 0.5rem; /* rounded-lg */
     color: #1f2937; /* text-gray-800 */
     box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1);
-    max-width: 32rem; /* max-w-lg */
+    max-width: 38rem; /* max-w-lg */
     margin-left: auto;
     margin-right: auto;
     margin-top: 2.5rem; /* my-10 */
     margin-bottom: 2.5rem;
   }
   .testimonial-card img {
-    width: 6rem; /* w-24 */
-    height: 6rem; /* h-24 */
+    width: 8rem; /* w-24 */
+    height: 8rem; /* h-24 */
     border-radius: 9999px; /* rounded-full */
     border: 0.5rem solid #facc15; /* border-4 border-yellow-300 */
   }
