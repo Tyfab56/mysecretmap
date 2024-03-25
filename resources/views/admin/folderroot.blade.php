4ac07555c040df5e0e7f3026e04adfa4c10673f7
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-03-25 21:04:46 +0400

    folder

--- resources/views/admin/folderroot.blade.php
+++ resources/views/admin/folderroot.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <h1>Gestion des Dossiers</h1>
     <p>Bienvenue sur la page de gestion des dossiers.</p>
-    <a href="{{ route('folders.index') }}" class="btn btn-primary">Voir les dossiers</a>
+    <a href="{{ route('admin.folders.index') }}" class="btn btn-primary">Voir les dossiers</a>
     <!-- Vous pouvez ajouter d'autres boutons ou liens ici -->
 </div>
 @endsection
