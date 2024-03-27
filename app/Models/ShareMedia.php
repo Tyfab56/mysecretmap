1c6eb56cfd4538b2e7796b79f5a0472b27108959
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-03-27 16:13:11 +0400

    medias

--- app/Models/ShareMedia.php
+++ app/Models/ShareMedia.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class ShareMedia extends Model
 {
     protected $table = 'sharemedias';
-    protected $fillable = ['folder_id', 'title', 'media_link', 'media_type', 'credits'];
+
+    protected $fillable = [
+        'folder_id', 'title', 'media_link', 'thumbnail_link', 'media_type',
+        // Ajoutez tous les autres champs que vous souhaitez pouvoir assigner en masse
+    ];
 }
 
