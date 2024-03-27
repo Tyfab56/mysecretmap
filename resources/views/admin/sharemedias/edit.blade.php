cca32bf4c6e2826115e91d8081803effd16c52fd
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-03-27 16:19:45 +0400

    medias

--- resources/views/admin/sharemedias/edit.blade.php
+++ resources/views/admin/sharemedias/edit.blade.php
@@ -7,36 +7,28 @@
     <form action="{{ route('admin.sharemedias.update', $shareMedia->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
+        
+        <!-- Autres champs du formulaire -->
+        
         <div class="form-group">
-            <label for="folder_id">Dossier</label>
-            <select class="form-control" id="folder_id" name="folder_id">
-                @foreach ($folders as $folder)
-                    <option value="{{ $folder->id }}" {{ $shareMedia->folder_id == $folder->id ? 'selected' : '' }}>{{ $folder->name }}</option>
-                @endforeach
-            </select>
+            <label>Vignette Actuelle</label>
+            <br>
+            <!-- Afficher la vignette actuelle s'il en existe une -->
+            @if($shareMedia->thumbnail_link)
+                <img src="{{ $shareMedia->thumbnail_link }}" alt="Vignette" style="max-width: 100px; max-height: 100px;">
+            @else
+                Pas de vignette disponible.
+            @endif
         </div>
+
         <div class="form-group">
-            <label for="title">Titre</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ $shareMedia->title }}" required>
+            <label for="thumbnail">Changer la Vignette</label>
+            <input type="file" class="form-control-file" id="thumbnail" name="thumbnail">
         </div>
-        <div class="form-group">
-            <label for="media_link">Lien du Média</label>
-            <input type="text" class="form-control" id="media_link" name="media_link" value="{{ $shareMedia->media_link }}" required>
-        </div>
-        <div class="form-group">
-            <label for="media_type">Type de Média</label>
-            <select class="form-control" id="media_type" name="media_type">
-                <option value="photo" {{ $shareMedia->media_type == 'photo' ? 'selected' : '' }}>Photo</option>
-                <option value="video" {{ $shareMedia->media_type == 'video' ? 'selected' : '' }}>Vidéo</option>
-                <option value="film" {{ $shareMedia->media_type == 'film' ? 'selected' : '' }}>Film</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="credits">Crédits</label>
-            <input type="number" class="form-control" id="credits" name="credits" value="{{ $shareMedia->credits }}">
-        </div>
+
+        <!-- Autres champs du formulaire -->
+        
         <button type="submit" class="btn btn-primary">Mettre à jour</button>
     </form>
 </div>
 @endsection
-
