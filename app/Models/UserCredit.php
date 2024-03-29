f46a49bf0c67f82dd8f158dc4b1dbdf987f6b068
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-03-29 17:53:22 +0400

    tail

--- app/Models/UserCredit.php
+++ app/Models/UserCredit.php
@@ -9,6 +9,8 @@ class UserCredit extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['user_id', 'media_type', 'credits'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
