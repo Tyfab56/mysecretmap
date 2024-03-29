f294be8041e781f353f9807a199742b063fdf398
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-03-29 16:54:21 +0400

    tail

--- resources/views/frontend/showbyfolder.blade.php
+++ resources/views/frontend/showbyfolder.blade.php
@@ -129,6 +129,9 @@
 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/4.2.2/masonry.pkgd.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/imagesloaded/4.1.4/imagesloaded.pkgd.min.js"></script>
+
 
 <script>
 var $grid;
