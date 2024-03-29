38dd8efad2f72e7bba380306b2bd424e94655cce
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-03-29 16:47:23 +0400

    tail

--- resources/views/frontend/showbyfolder.blade.php
+++ resources/views/frontend/showbyfolder.blade.php
@@ -127,13 +127,13 @@
 @endsection
 
 @push('scripts')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/4.2.2/masonry.pkgd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/imagesloaded/4.1.4/imagesloaded.pkgd.min.js"></script>
 <script>
-
+var $grid;
 $(document).ready(function() {
-    var $grid = $('#gallery-wrapper').imagesLoaded(function() {
+    $grid = $('#gallery-wrapper').imagesLoaded(function() {
     $grid.masonry({
         itemSelector: '.picture-item',
         percentPosition: true,
