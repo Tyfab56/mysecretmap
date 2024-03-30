06d57c04e5341d89a96bf9a44c4ca0bee8ba65b2
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-03-30 19:00:08 +0400

    medias

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Folder;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Pays;
+use App\Models\Pays;
 
 class FolderController extends Controller
 {
