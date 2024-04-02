c29f1acc1d3031ad2cbaec6b23bc632663b40309
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-04-02 10:29:05 +0400

    account

--- resources/views/frontend/mesmedias.blade.php
+++ resources/views/frontend/mesmedias.blade.php
@@ -6,7 +6,7 @@
 
 <div class="btn-group mb-3">
     <a href="{{ route('mes-medias')}}" class="mr-4 btn btn-primary">Mes Médias</a>
-    <a href="" class="mr-4 btn btn-secondary">Médias Privés</a>
+    <a href="{{ route('rivate-folders')}}"" class="mr-4 btn btn-secondary">Médias Privés</a>
     <a href="{{ route('public-folders')}}" class="btn btn-primary">Médias Publics</a>
 </div>
 
