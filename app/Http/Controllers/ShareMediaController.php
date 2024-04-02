bc544879c600b5fbe10d5dc96d0936eca72bed03
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-04-02 12:30:09 +0400

    account

--- app/Http/Controllers/ShareMediaController.php
+++ app/Http/Controllers/ShareMediaController.php
@@ -173,6 +173,7 @@ class ShareMediaController extends Controller
     
         // Vérifier si l'utilisateur est admin ou a accès au dossier
         $user = Auth::user();
+        dd($folder->status );
         // Check if the folder is private before applying the access restrictions
         if ($folder->status == 'private' && !$user->isAdmin() && !$folder->users->contains('id', $user->id)) {
             abort(403, "Vous n'avez pas l'autorisation d'accéder à ce dossier.");
