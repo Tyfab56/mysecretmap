b7a7816aca242a7fd82396b6879371a98eb0cf53
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-04-02 10:50:19 +0400

    account

--- resources/views/frontend/privatefolders.blade.php
+++ resources/views/frontend/privatefolders.blade.php
@@ -4,15 +4,18 @@
 <div class="container mt-4">
 <h1>Dossiers Privés</h1>
    @include('partials.navmedias')
+   <div class="row">
     @foreach($privateFolders as $folder)
-        <div class="card" style="width: 18rem; float: left; margin: 10px;">
-            <img src="{{ $folder->shareMedias->first()->thumbnail_link ?? 'placeholder-image-url' }}" class="card-img-top" alt="Thumbnail">
-            <div class="card-body">
-                <h5 class="card-title">{{ $folder->name }}</h5>
-                <p class="card-text">Nombre de médias : {{ $folder->shareMedias->count() }}</p>
-                <a href="" class="btn btn-primary">Voir le dossier</a>
+    <div class="card" style="width: 18rem; float: left; margin: 10px;">
+                <img src="{{ $folder->shareMedias->first()->thumbnail_link ?? 'placeholder-image-url' }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $folder->name }}</h5>
+                    <p class="card-text">Nombre de médias : {{ $folder->shareMedias->count() }}</p>
+                    <a href="" class="btn btn-primary">Voir le dossier</a>
+                </div>
             </div>
         </div>
     @endforeach
+</div>
 </div>
 @endsection
