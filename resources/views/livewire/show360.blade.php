14fc74b26541b8e6815b2cd3f6de36e060f96f53
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-05-12 23:16:39 +0400

    message

--- resources/views/livewire/show360.blade.php
+++ resources/views/livewire/show360.blade.php
@@ -3,6 +3,8 @@
         <div id="panorama" style="width: 100%; height: 500px;"></div>
     @endif
     <script>
-        console.log("Direct console log");
+         document.addEventListener('livewire:load', function() {
+        console.log("Livewire is fully loaded.");
+    });
     </script>
 </div>
