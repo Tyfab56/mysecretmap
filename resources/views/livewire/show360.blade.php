8578a677ef3cceb423db46f5a7d28d3ea5223104
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-05-12 23:07:50 +0400

    message

--- resources/views/livewire/show360.blade.php
+++ resources/views/livewire/show360.blade.php
@@ -2,35 +2,20 @@
     @if($spot && !is_null($spot->img360))
         <div id="panorama" style="width: 100%; height: 500px;"></div>
     @endif   
-    @push('styles')
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pannellum/build/pannellum.css"/>
-    @endpush
-
     @push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/pannellum/build/pannellum.js"></script>
-<script>
-    function initializePanorama(imageUrl) {
-        if (imageUrl && imageUrl.length > 0) {
-            pannellum.viewer('panorama', {
-                "type": "equirectangular",
-                "panorama": imageUrl,
-                "autoLoad": true
-            });
-        }
-    }
+    <script>
+        document.addEventListener('livewire:load', function() {
+            @if($spot)
+            console.log("Livewire:load - Spot object:", @json($spot));  // Log the entire $spot object
+            @else
+            console.log("Livewire:load - No spot object available");
+            @endif
+        });
 
-    document.addEventListener('livewire:load', function() {
-        console.log("Livewire:load - Spot object:", @json($spot));
-        @if($spot && !is_null($spot->img360))
-        initializePanorama("{{ asset('storage/' . $spot->img360) }}");
-        @endif
-    });
-
-    Livewire.on('updatePanorama', imageUrl => {
-        initializePanorama(imageUrl);
-    });
-</script>
-@endpush
-
+        Livewire.on('updatePanorama', imageUrl => {
+            console.log("Received new image URL via Livewire.on('updatePanorama'):", imageUrl);
+        });
+    </script>
+    @endpush
 </div>
 
