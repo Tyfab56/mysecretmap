1a21e19008d22fa7b673cb2cedd0c07c5e4e1d39
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-05-12 22:50:56 +0400

    message

--- app/Http/Livewire/Show360.php
+++ app/Http/Livewire/Show360.php
@@ -15,9 +15,10 @@ class Show360 extends Component
     // Renommage de la méthode pour suivre la convention camelCase
     public function img360($idspot)
     {
-        dd($idspot);
+       
         if (!is_null($idspot)) {
             $this->idspot = $idspot;
+            dd($this->idspot);
         }
     }
 
