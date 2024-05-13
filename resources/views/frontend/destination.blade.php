e25912bb43d92f0ba0964b64acb4af89aeca4438
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-05-13 13:14:37 +0400

    message

--- resources/views/frontend/destination.blade.php
+++ resources/views/frontend/destination.blade.php
@@ -90,7 +90,7 @@
                                         </div>  
                                         <div class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">  
                                           <div class="col-lg-12 center pt-1 pb-1 pr-1 pl-1"><livewire:show360 /></div>
-                                          <div id="panorama"></div>
+                                         
                                           <script>
 document.addEventListener('DOMContentLoaded', function() {
     initializePannellum();
