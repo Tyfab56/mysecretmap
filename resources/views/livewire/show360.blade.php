6ec8a244727e011989de42ef3cd343780fc3148c
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-05-13 12:19:16 +0400

    message

--- resources/views/livewire/show360.blade.php
+++ resources/views/livewire/show360.blade.php
@@ -1,6 +1,7 @@
 <div>
 @if($spot && !is_null($spot->img360))
         <img id="panorama" src="{{ $spot->img360 }}" alt="360 View" style="width: 100%; height: auto;" data-pano-src="{{ $spot->img360 }}">
+        <div id="panorama" style="width: 100%; height: 500px;" data-pano-src="{{ $spot->img360 }}"></div>
     @endif
 
 </div>
