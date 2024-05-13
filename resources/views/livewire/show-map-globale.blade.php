8026b6b46f48a452e3ccbd7f7a5831e54f05b682
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-05-13 23:48:21 +0400

    message

--- resources/views/livewire/show-map-globale.blade.php
+++ resources/views/livewire/show-map-globale.blade.php
@@ -1,20 +1,17 @@
 <div class="img-container">
 
     @isset($spot->videomap)
-<video  class="videomap br5" src="{{$spot->videomap}}" autoplay loop muted nocontrols></video>
+    <video class="videomap br5" src="{{$spot->videomap}}" autoplay loop muted nocontrols></video>
     @else
     @isset ($spot->imgmapmedium)
-            @empty($spot->imgmapmedium) 
-            <img class="br5 img-image" src="{{asset('frontend/assets/images/nopic.jpg')}}">
-             <div class="img-overlay">{{ __('destination.ImageMissing') }} : Map Global Image ({{$spot->id}})</div>
-            @else
-                <img class="br5 img-image" src="{{$spot->imgmapmedium??''}}">  
-                <div class="img-overlay">{{ __('destination.SpotGlobal') }}</div>     
-            @endempty
+    @empty($spot->imgmapmedium)
+    <img class="br5 img-image" src="{{asset('frontend/assets/images/nopic.jpg')}}">
+    <div class="img-overlay">{{ __('destination.ImageMissing') }} : Map Global Image ({{$spot->id}})</div>
     @else
-           <img class="br5 img-image" src="{{asset('frontend/assets/images/clickmap.jpg')}}">
-           <div class="img-overlay">{{ __('destination.SpotClick') }}</div>
+    <img class="br5 img-image" src="{{$spot->imgmapmedium??''}}">
+    <div class="img-overlay">{{ __('destination.SpotGlobal') }}</div>
+    @endempty
 
-    @endif 
-  @endif
+
+    @endif
 </div>
