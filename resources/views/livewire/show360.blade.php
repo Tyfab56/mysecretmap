bc99fb4d055468ba7e48f305d8c90b36ec94df32
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-05-13 11:06:12 +0400

    message

--- resources/views/livewire/show360.blade.php
+++ resources/views/livewire/show360.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if($spot && !is_null($spot->img360))
-            <div id="panorama" style="width: 100%; height: 500px;"></div>
+        <img src="{{ $spot->img360 }}" alt="360 View" style="width: 100%; height: auto;">
     @endif
 
 @push('styles')
@@ -10,15 +10,5 @@
 @push('scripts')
 <script src="https://cdn.jsdelivr.net/npm/pannellum/build/pannellum.js"></script>
 @endpush
-<script>
-document.addEventListener('livewire:load', function() {
-    console.log("Spot object on load:", @json($spot));
-    pannellum.viewer('panorama', {
-        "type": "equirectangular",  // Type de panorama, assurez-vous que cela correspond à votre image
-        "panorama": "",
-        "autoLoad": true  // Charge automatiquement le panorama
-    });
-});
-</script>
 
 </div>
