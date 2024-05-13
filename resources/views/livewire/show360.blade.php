a9126a89ff03601b6a97be00ec527f0eb9413f56
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-05-13 11:30:01 +0400

    message

--- resources/views/livewire/show360.blade.php
+++ resources/views/livewire/show360.blade.php
@@ -3,13 +3,6 @@
         <img src="{{ $spot->img360 }}" alt="360 View" style="width: 100%; height: auto;">
     @endif
 
-@push('styles')
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pannellum/build/pannellum.css"/>
-@endpush
-
-@push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/pannellum/build/pannellum.js"></script>
-@endpush
 <div id="panorama"></div>
 <div wire:loading.remove>
 <script>
