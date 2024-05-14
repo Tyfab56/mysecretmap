d3f6be10854ce788d8d0236578161429d621f24a
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-05-14 14:27:30 +0400

    message

--- resources/views/frontend/destination.blade.php
+++ resources/views/frontend/destination.blade.php
@@ -47,6 +47,10 @@
         </div>
 
         <div class="row">
+            <div class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">
+                <div class="col-lg-12 pt-1 pb-1  pr-1 pl-1"><livewire:show-map-globale />
+                </div>
+            </div>
             <div class="col-lg-12">
                 <div class="row">
                     <div class="row bgregbox w100">
@@ -135,10 +139,7 @@
                             </div>
 
 
-                            <div class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">
-                                <div class="col-lg-12 pt-1 pb-1  pr-1 pl-1"><livewire:show-map-globale />
-                                </div>
-                            </div>
+
                             <div id="wrap_video" class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">
                                 <div id="container_video" style="width: 640px"></div>
                             </div>
