b74776fb03235402947ce0e5f1c786f4d382a5ba
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-01 09:50:28 +0400

    banner

--- resources/views/auth/login2.blade.php
+++ resources/views/auth/login2.blade.php
@@ -4,15 +4,15 @@
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-6">
-            <div class="card shadow-lg border-0 rounded-lg mt-5">
+            <div class="card shadow-lg border-0 rounded-lg">
                 <div class="card-header bg-primary text-white">
-                    <h3 class="text-center font-weight-light my-4">{{ __('Login') }}</h3>
+                    <h3 class="text-center font-weight-light my-4">{{ __('auth.login') }}</h3>
                 </div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
                         <div class="form-group">
-                            <label for="email" class="small mb-1">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="small mb-1">{{ __('auth.email') }}</label>
                             <input id="email" type="email" class="form-control py-4 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             @error('email')
                             <span class="invalid-feedback" role="alert">
@@ -21,7 +21,7 @@
                             @enderror
                         </div>
                         <div class="form-group">
-                            <label for="password" class="small mb-1">{{ __('Password') }}</label>
+                            <label for="password" class="small mb-1">{{ __('auth.password') }}</label>
                             <input id="password" type="password" class="form-control py-4 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                             @error('password')
                             <span class="invalid-feedback" role="alert">
@@ -32,21 +32,21 @@
                         <div class="form-group">
                             <div class="custom-control custom-checkbox">
                                 <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                <label class="custom-control-label" for="remember">{{ __('Remember Me') }}</label>
+                                <label class="custom-control-label" for="remember">{{ __('auth.remember_me') }}</label>
                             </div>
                         </div>
                         <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
                             @if (Route::has('password.request'))
-                            <a class="small" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+                            <a class="small" href="{{ route('password.request') }}">{{ __('auth.forgot_password') }}</a>
                             @endif
-                            <button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
+                            <button type="submit" class="btn btn-primary">{{ __('auth.login') }}</button>
                         </div>
                     </form>
                 </div>
                 <div class="card-footer text-center">
                     @if (Route::has('register'))
                     <div class="small">
-                        <a href="{{ route('register') }}">{{ __("Don't have an account? Register") }}</a>
+                        <a href="{{ route('register') }}">{{ __('auth.no_account') }}</a>
                     </div>
                     @endif
                 </div>
