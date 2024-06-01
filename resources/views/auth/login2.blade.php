745312edc89148214aaa67d722d89fd8331ce3fe
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-01 09:54:32 +0400

    banner

--- resources/views/auth/login2.blade.php
+++ resources/views/auth/login2.blade.php
@@ -1,56 +1,58 @@
 @extends('frontend.main_master')
 
 @section('content')
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card shadow-lg border-0 rounded-lg">
-                <div class="card-header bg-primary text-white">
-                    <h3 class="text-center font-weight-light my-4">{{ __('auth.login') }}</h3>
+<div class="font-sans text-gray-900 antialiased">
+    <div class="flex flex-col items-center pt-5 sm:pt-0 bg-gray-100 pb-5">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
+
+            <!-- Validation Errors -->
+            <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <!-- Email Address -->
+                <div>
+                    <x-label for="email" :value="__('auth.email')" />
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
                 </div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="form-group">
-                            <label for="email" class="small mb-1">{{ __('auth.email') }}</label>
-                            <input id="email" type="email" class="form-control py-4 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                            @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="password" class="small mb-1">{{ __('auth.password') }}</label>
-                            <input id="password" type="password" class="form-control py-4 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <div class="custom-control custom-checkbox">
-                                <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                <label class="custom-control-label" for="remember">{{ __('auth.remember_me') }}</label>
-                            </div>
-                        </div>
-                        <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
-                            @if (Route::has('password.request'))
-                            <a class="small" href="{{ route('password.request') }}">{{ __('auth.forgot_password') }}</a>
-                            @endif
-                            <button type="submit" class="btn btn-primary">{{ __('auth.login') }}</button>
-                        </div>
-                    </form>
+
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-label for="password" :value="__('auth.password')" />
+                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
                 </div>
-                <div class="card-footer text-center">
-                    @if (Route::has('register'))
-                    <div class="small">
-                        <a href="{{ route('register') }}">{{ __('auth.no_account') }}</a>
-                    </div>
+
+                <!-- Remember Me -->
+                <div class="block mt-4">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring focus:ring-indigo-500" name="remember">
+                        <span class="ml-2 text-sm text-gray-600">{{ __('auth.remember_me') }}</span>
+                    </label>
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('auth.forgot_password') }}
+                    </a>
                     @endif
+
+                    <x-button class="ml-3">
+                        {{ __('auth.login') }}
+                    </x-button>
                 </div>
-            </div>
+            </form>
+        </div>
+
+        <div class="btn-center mt-4">
+            <a href="{{ route('register') }}">
+                <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    {{ __('auth.register') }}
+                </button>
+            </a>
         </div>
     </div>
 </div>
