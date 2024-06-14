3ec2d0e34ca23ac4ed28fdba91ea148bb4784e87
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-14 12:38:21 +0400

    checkbox

--- resources/views/frontend/destination.blade.php
+++ resources/views/frontend/destination.blade.php
@@ -58,10 +58,10 @@
                         <div class="col-lg-12">
                             <p>{{__('destination.clickmap')}}</p>
                             <div style="display: flex; gap: 10px; align-items: center;">
-                                <label><input type="checkbox" id="1" name="spotType" value="1" checked> Spots</label>
-                                <label><input type="checkbox" id="3" name="spotType" value="3" checked> Musées</label>
-                                <label><input type="checkbox" id="4" name="spotType" value="4"> Hotels</label>
-                                <label><input type="checkbox" id="5" name="spotType" value="5"> Campings</label>
+                                <label><input type="checkbox" id="1" name="spotType" value="1" checked> {{__('destination.Spot')}}Spots</label>
+                                <label><input type="checkbox" id="3" name="spotType" value="3"> {{__('destination.Musee')}}Musées</label>
+                                <label><input type="checkbox" id="4" name="spotType" value="4"> {{__('destination.Hotel')}}Hotels</label>
+                                <label><input type="checkbox" id="5" name="spotType" value="5"> {{__('destination.Camping')}}Campings</label>
                                 <!-- Add more checkboxes as needed -->
                             </div>
                             <div class="leaflet-map">
