2a3fae8c1eb8ccc68fef6c899f692c196b787650
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-14 11:20:00 +0400

    checkbox

--- resources/views/frontend/mapdestinationjs.blade.php
+++ resources/views/frontend/mapdestinationjs.blade.php
@@ -542,7 +542,7 @@ while (index < str.length) { // Reset shift, result, and byte byte=null; shift=0
     checkbox.addEventListener('change', function() {
     const selectedTypes = Array.from(document.querySelectorAll('input[name="spotType"]:checked'))
     .map(cb => cb.value);
-    fetch("/spots?maps_id=${selectedTypes.join(',')}")
+    fetch(`/spots?maps_id=${selectedTypes.join(',')}`)
     .then(response => response.json())
     .then(data => {
     updateMap(data);
