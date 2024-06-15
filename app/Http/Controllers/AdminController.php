8ed0be40e5bc268b9396f20a283535480a92d78c
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-15 10:47:36 +0400

    checkbox

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -68,7 +68,7 @@ class AdminController extends Controller
 
         // Tri des spots pour un parcours logique en utilisant les distances pré-calculées
         $sortedSpots = $this->sortSpots($spotsWithTranslations, $defaultSpot->spot_id);
-        dump($sortedSpots);
+        dd($sortedSpots);
         // Vider la table des spots triés pour ce pays
         SortedSpot::where('pays_id', $countryId)->delete();
 
