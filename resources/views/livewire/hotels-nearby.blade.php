f233d646ea1c39d0794d070ce24f94a5c9e4db9c
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-17 12:50:22 +0400

    checkbox

--- resources/views/livewire/hotels-nearby.blade.php
+++ resources/views/livewire/hotels-nearby.blade.php
@@ -14,70 +14,69 @@
         @endforeach
     </div>
     @endif
+    <style>
+        .hotel-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+        }
+
+        .hotel-box {
+            flex: 1 1 20%;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            margin: 10px;
+            background-color: #ffffff;
+            box-sizing: border-box;
+        }
+
+        .hotel-link {
+            display: inline-block;
+            width: 100%;
+            color: inherit;
+            text-decoration: none;
+        }
+
+        .hotel-image {
+            width: 100%;
+            height: auto;
+        }
+
+        .hotel-content {
+            padding: 10px;
+        }
+
+        .hotel-content h3 {
+            margin: 0;
+            font-size: 16px;
+        }
+
+        .hotel-content p {
+            margin: 0;
+            font-size: 14px;
+        }
+
+        @media (max-width: 1200px) {
+            .hotel-box {
+                flex: 1 1 25%;
+            }
+        }
+
+        @media (max-width: 992px) {
+            .hotel-box {
+                flex: 1 1 33.33%;
+            }
+        }
+
+        @media (max-width: 768px) {
+            .hotel-box {
+                flex: 1 1 45%;
+            }
+        }
+
+        @media (max-width: 576px) {
+            .hotel-box {
+                flex: 1 1 100%;
+            }
+        }
+    </style>
 </div>
-
-<style>
-    .hotel-container {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-    }
-
-    .hotel-box {
-        flex: 1 1 20%;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        margin: 10px;
-        background-color: #ffffff;
-        box-sizing: border-box;
-    }
-
-    .hotel-link {
-        display: inline-block;
-        width: 100%;
-        color: inherit;
-        text-decoration: none;
-    }
-
-    .hotel-image {
-        width: 100%;
-        height: auto;
-    }
-
-    .hotel-content {
-        padding: 10px;
-    }
-
-    .hotel-content h3 {
-        margin: 0;
-        font-size: 16px;
-    }
-
-    .hotel-content p {
-        margin: 0;
-        font-size: 14px;
-    }
-
-    @media (max-width: 1200px) {
-        .hotel-box {
-            flex: 1 1 25%;
-        }
-    }
-
-    @media (max-width: 992px) {
-        .hotel-box {
-            flex: 1 1 33.33%;
-        }
-    }
-
-    @media (max-width: 768px) {
-        .hotel-box {
-            flex: 1 1 45%;
-        }
-    }
-
-    @media (max-width: 576px) {
-        .hotel-box {
-            flex: 1 1 100%;
-        }
-    }
-</style>
