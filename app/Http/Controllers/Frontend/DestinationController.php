866d1637701d568669318e438ce6a1df13aad038
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-17 10:55:39 +0400

    checkbox

--- app/Http/Controllers/Frontend/DestinationController.php
+++ app/Http/Controllers/Frontend/DestinationController.php
@@ -9,6 +9,7 @@ use App\Models\Circuits;
 use App\Models\Circuits_details;
 use App\Models\Pictures;
 use App\Models\Default_spots;
+use App\Models\Region;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
