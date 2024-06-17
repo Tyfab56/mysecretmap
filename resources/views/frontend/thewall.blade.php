eda0b80b3b3ee2bce4338fc3eed93d5bc6244dda
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-17 12:17:13 +0400

    checkbox

--- resources/views/frontend/thewall.blade.php
+++ resources/views/frontend/thewall.blade.php
@@ -87,7 +87,7 @@ window.location.href = "{{ url('thewall') }}/{{ $idpays }}/" + this.id;
   }
 
   .img-wall {
-    width: 100%;
+
     height: 100%;
     object-fit: cover;
     border-radius: 5px;
