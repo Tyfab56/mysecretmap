e86844fc28b8f25b3d289ec43f4fd803dbd1ebdb
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-08-19 15:58:50 +0400

    appli

--- resources/views/livewire/spot-banner.blade.php
+++ resources/views/livewire/spot-banner.blade.php
@@ -1,11 +1,11 @@
 <div>
-    <div class="d-flex justify-content-center align-items-center" style=" min-height: 100px;">
+    <div class="d-flex justify-content-center align-items-center bg-light p-3" style="min-height: 100px; display: inline-block;">
         @if ($banner)
         <a href="{{ $banner->redirect_url }}" target="_blank">
-            <img src="{{ $banner->image_url }}" alt="Publicité" class="img-fluid w100">
+            <img src="{{ $banner->image_url }}" alt="Publicité" class="img-fluid w-100">
         </a>
         @else
-        <img src="{{ asset('frontend/assets/images/blog/pub1.jpg')}}" alt="Publicité" class="img-fluid w100">
+        <img src="{{ asset('frontend/assets/images/blog/pub1.jpg')}}" alt="Publicité" class="img-fluid w-100">
         @endif
     </div>
 </div>
