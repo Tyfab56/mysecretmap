c8bd52a11deea9ea352b992c862edad0437890b0
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-08-19 21:34:36 +0400

    appli

--- resources/views/frontend/destination.blade.php
+++ resources/views/frontend/destination.blade.php
@@ -149,20 +149,12 @@
                                 </div>
 
 
-                                <div class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">
-                                    <div class="col-lg-12 center pt-1 pb-1 pr-1 pl-1">
-                                        <livewire:show-map-globale />
-                                    </div>
-                                </div>
+
                                 <!-- Affichage video -->
                                 <div id="wrap_video" class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">
                                     <div id="container_video"></div>
                                 </div>
-                                <!-- Template styles-->
-                                <div class="row pt-1 pb-1 pr-1 pl-1 bgregbox min100">
-                                    <div class="col-lg-12 center pt-1 pb-1 pr-1 pl-1"><livewire:show-img-region />
-                                    </div>
-                                </div>
+
                             </div>
                         </div>
                     </div>
--- resources/views/livewire/slider-pictures.blade.php
+++ resources/views/livewire/slider-pictures.blade.php
@@ -1,30 +1,30 @@
+@if($pictures->isNotEmpty())
 <div>
   <h1>Pictures about ({{$pictures->count()}})</h1>
-<div class="gridOverflow go-masonry">
+  <div class="gridOverflow go-masonry">
 
     @foreach ($pictures as $picture)
-    <a href="/destination/{{$picture->spot->pays_id}}/{{$picture->spot->id}}"class="go_gridItem">
+    <a href="/destination/{{$picture->spot->pays_id}}/{{$picture->spot->id}}" class="go_gridItem">
 
-         <img src="{{ $picture->medium}}" /> 
-         <span class="go_caption go_caption-full">
-                {{ $picture->spot->name}}
-                <img src="{{ $picture->user->avatar}}" class="avatar-r45" alt="{{$picture->user->pseudo}}"/> 
-         </span>
-         
-            
+      <img src="{{ $picture->medium}}" />
+      <span class="go_caption go_caption-full">
+        {{ $picture->spot->name}}
+        <img src="{{ $picture->user->avatar}}" class="avatar-r45" alt="{{$picture->user->pseudo}}" />
+      </span>
+
     </a>
     @endforeach
-  <div class="go_gridItem go_gridItem-centered" href="someURL"><p> </p> </div>
-  @if($pictures->isNotEmpty())
+    <div class="go_gridItem go_gridItem-centered" href="someURL">
+      <p> </p>
+    </div>
     <a href="{{ route('gallery', ['idspot' => $pictures->first()->spot_id]) }}" class="btn btn-primary">
-       {{__('destination.CompleteGallery')}} 
+      {{__('destination.CompleteGallery')}}
     </a>
-  @endif
-  @if(auth()->user() && auth()->user()->isPhotographer())
-        <a href="" class="btn btn-secondary">
-            Proposer des photos
-        </a>
+    @if(auth()->user() && auth()->user()->isPhotographer())
+    <a href="" class="btn btn-secondary">
+      Proposer des photos
+    </a>
     @endif
+  </div>
 </div>
-
-</div>
+@endif
