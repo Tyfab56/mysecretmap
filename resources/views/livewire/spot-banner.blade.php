25fb7deaeb91144e53b839c26005874fb1c0c43c
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-08-19 15:50:01 +0400

    appli

--- resources/views/livewire/spot-banner.blade.php
+++ resources/views/livewire/spot-banner.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-12 col-md-10">
-            <div class="d-flex justify-content-center align-items-center bg-light p-3" style="min-height: 100px;">
+            <div class="bg-light p-3" style="text-align: center; min-height: 100px;">
                 @if ($banner)
                 <a href="{{ $banner->redirect_url }}" target="_blank">
                     <img src="{{ $banner->image_url }}" alt="Publicité" class="img-fluid w-100">
