bec97acc070edf190ac77b5b71e00281e17306df
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-09-23 10:20:53 +0400

    photographe

--- resources/views/frontend/photographers.blade.php
+++ resources/views/frontend/photographers.blade.php
@@ -1,14 +1,15 @@
 @extends('frontend.main_master')
 @section('content')
 <section class="fdb-block py-0">
-  <div class="container bg-r py-5 my-5" style="background-image: url(imgs/shapes/1.svg);">
+  <div class="container bg-r py-5 my-5" style="background-image: url( {{asset('frontend/assets/images/mockup-phone1.png')}} );">
     <div class="row py-5">
       <div class="col-12 col-sm-10 col-md-8 col-lg-6 text-left">
         <h1>{{ __('photo.Arg1') }}</h1>
         <p class="lead">
-         {{ __('photo.Arg2') }}   </p>
+          {{ __('photo.Arg2') }}
+        </p>
         <p class="mt-4">
-        <a href="https://mysecretmap.com/contact" class="btn">{{ __('photo.Arg3') }} </a>
+          <a href="https://mysecretmap.com/contact" class="btn">{{ __('photo.Arg3') }} </a>
         </p>
       </div>
     </div>
@@ -17,7 +18,7 @@
 <div class="container">
 
   <p class="intro-text">
-   
+
   </p>
 
   <div class="section">
