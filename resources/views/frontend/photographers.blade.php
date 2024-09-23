9ea94c6f4da2eb9d952a2ee2b5e6cf153bcc058c
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-09-23 11:53:18 +0400

    photographe

--- resources/views/frontend/photographers.blade.php
+++ resources/views/frontend/photographers.blade.php
@@ -1,6 +1,12 @@
 @extends('frontend.main_master')
 @section('content')
 
+<head>
+  <!-- Lien vers Font Awesome pour les icônes -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+</head>
+
+<!-- Section Titre principal -->
 <section style="background-color: #f7f7f7;">
   <div class="container py-4 my-4">
     <div class="row align-items-center py-3">
@@ -15,14 +21,16 @@
 <!-- Section Visibilité -->
 <section style="background-color: #f0f0f0;">
   <div class="container py-4">
-    <div class="row">
-      <div class="col-12 col-lg-6 text-left">
-        <h2>{{ __('photo.visibility_title') }}</h2>
-        <ul>
-          <li>{{ __('photo.visibility_1') }}</li>
-          <li>{{ __('photo.visibility_2') }}</li>
-          <li>{{ __('photo.visibility_3') }}</li>
-        </ul>
+    <div class="card shadow-sm p-4 mb-4">
+      <div class="row">
+        <div class="col-12 col-lg-6 text-left">
+          <h2><i class="fas fa-eye"></i> {{ __('photo.visibility_title') }}</h2>
+          <ul>
+            <li><i class="fas fa-bullhorn"></i> {{ __('photo.visibility_1') }}</li>
+            <li><i class="fas fa-link"></i> {{ __('photo.visibility_2') }}</li>
+            <li><i class="fas fa-globe"></i> {{ __('photo.visibility_3') }}</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -31,14 +39,16 @@
 <!-- Section Revenus -->
 <section style="background-color: #f7f7f7;">
   <div class="container py-4">
-    <div class="row">
-      <div class="col-12 col-lg-6 text-left">
-        <h2>{{ __('photo.revenue_title') }}</h2>
-        <ul>
-          <li>{{ __('photo.revenue_1') }}</li>
-          <li>{{ __('photo.revenue_2') }}</li>
-          <li>{{ __('photo.revenue_3') }}</li>
-        </ul>
+    <div class="card shadow-sm p-4 mb-4">
+      <div class="row">
+        <div class="col-12 col-lg-6 text-left">
+          <h2><i class="fas fa-dollar-sign"></i> {{ __('photo.revenue_title') }}</h2>
+          <ul>
+            <li><i class="fas fa-chart-line"></i> {{ __('photo.revenue_1') }}</li>
+            <li><i class="fas fa-file-invoice-dollar"></i> {{ __('photo.revenue_2') }}</li>
+            <li><i class="fas fa-hand-holding-usd"></i> {{ __('photo.revenue_3') }}</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -47,13 +57,15 @@
 <!-- Section Opportunité -->
 <section style="background-color: #f0f0f0;">
   <div class="container py-4">
-    <div class="row">
-      <div class="col-12 col-lg-6 text-left">
-        <h2>{{ __('photo.opportunity_title') }}</h2>
-        <ul>
-          <li>{{ __('photo.opportunity_1') }}</li>
-          <li>{{ __('photo.opportunity_2') }}</li>
-        </ul>
+    <div class="card shadow-sm p-4 mb-4">
+      <div class="row">
+        <div class="col-12 col-lg-6 text-left">
+          <h2><i class="fas fa-briefcase"></i> {{ __('photo.opportunity_title') }}</h2>
+          <ul>
+            <li><i class="fas fa-expand"></i> {{ __('photo.opportunity_1') }}</li>
+            <li><i class="fas fa-handshake"></i> {{ __('photo.opportunity_2') }}</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -62,16 +74,18 @@
 <!-- Section Agence -->
 <section style="background-color: #f7f7f7;">
   <div class="container py-4">
-    <div class="row">
-      <div class="col-12 col-lg-12 text-left">
-        <h2>{{ __('photo.agence_title') }}</h2>
-        <ul>
-          <li>{{ __('photo.agence_1') }}</li>
-          <li>{{ __('photo.agence_2') }}</li>
-          <li>{{ __('photo.agence_3') }}</li>
-          <li>{{ __('photo.agence_4') }}</li>
-          <li>{{ __('photo.agence_5') }}</li>
-        </ul>
+    <div class="card shadow-sm p-4 mb-4">
+      <div class="row">
+        <div class="col-12 col-lg-12 text-left">
+          <h2><i class="fas fa-building"></i> {{ __('photo.agence_title') }}</h2>
+          <ul>
+            <li><i class="fas fa-cogs"></i> {{ __('photo.agence_1') }}</li>
+            <li><i class="fas fa-globe"></i> {{ __('photo.agence_2') }}</li>
+            <li><i class="fas fa-bullhorn"></i> {{ __('photo.agence_3') }}</li>
+            <li><i class="fas fa-plane"></i> {{ __('photo.agence_4') }}</li>
+            <li><i class="fas fa-award"></i> {{ __('photo.agence_5') }}</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -80,16 +94,18 @@
 <!-- Section Contribution -->
 <section style="background-color: #f0f0f0;">
   <div class="container py-4">
-    <div class="row">
-      <div class="col-12 col-lg-6 text-left">
-        <h2>{{ __('photo.contribution_title') }}</h2>
-        <ul>
-          <li>{{ __('photo.contribution_1') }}</li>
-          <li>{{ __('photo.contribution_2') }}</li>
-          <li>{{ __('photo.contribution_3') }}</li>
-          <li>{{ __('photo.contribution_4') }}</li>
-          <li>{{ __('photo.contribution_5') }}</li>
-        </ul>
+    <div class="card shadow-sm p-4 mb-4">
+      <div class="row">
+        <div class="col-12 col-lg-6 text-left">
+          <h2><i class="fas fa-camera"></i> {{ __('photo.contribution_title') }}</h2>
+          <ul>
+            <li><i class="fas fa-photo-video"></i> {{ __('photo.contribution_1') }}</li>
+            <li><i class="fas fa-video"></i> {{ __('photo.contribution_2') }}</li>
+            <li><i class="fas fa-vr-cardboard"></i> {{ __('photo.contribution_3') }}</li>
+            <li><i class="fas fa-file-alt"></i> {{ __('photo.contribution_4') }}</li>
+            <li><i class="fas fa-map-marker-alt"></i> {{ __('photo.contribution_5') }}</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -98,10 +114,12 @@
 <!-- Section Traducteur -->
 <section style="background-color: #f7f7f7;">
   <div class="container py-4">
-    <div class="row">
-      <div class="col-12 col-lg-6 text-left">
-        <h2>{{ __('photo.translator_title') }}</h2>
-        <p>{{ __('photo.translator_desc') }}</p>
+    <div class="card shadow-sm p-4 mb-4">
+      <div class="row">
+        <div class="col-12 col-lg-6 text-left">
+          <h2><i class="fas fa-language"></i> {{ __('photo.translator_title') }}</h2>
+          <p>{{ __('photo.translator_desc') }}</p>
+        </div>
       </div>
     </div>
   </div>
