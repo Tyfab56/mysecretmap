0f1f059a08f5ccce05cedff81b4a63efadf34ef9
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-09-28 15:41:32 +0400

    guide

--- resources/views/guide/index.blade.php
+++ resources/views/guide/index.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guide for {{ $country_code }} - {{ $lang }}</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/framework7@7.0.0/css/framework7.bundle.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/framework7/framework7-bundle.min.css">
 </head>
 
 <body>
@@ -14,7 +14,7 @@
         <!-- This will be replaced by Framework7 components -->
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/framework7@7.0.0/js/framework7.bundle.min.js"></script>
+    <script src="https://unpkg.com/framework7/framework7-bundle.min.js"></script>
 
     <script>
         var app = new Framework7({
