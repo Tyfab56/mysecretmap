6b0f2e2f1acaabc9f19d5dfe5863d5444e783b01
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-09-29 11:43:33 +0400

    guide

--- public/guide_iceland_fr/service-worker.js
+++ public/guide_iceland_fr/service-worker.js
@@ -5,12 +5,14 @@ const CACHE_NAME = "v1";
 
 // Liste des fichiers à mettre en cache
 const urlsToCache = [
-    "/",
-    "/index.html",
-    "/pages/main.html",
-    "/css/style.css",
-    "/js/app.js",
-    "/images/maplogo.gif",
+    const urlsToCache = [
+        '/guide_iceland_fr/',  // Chemin correct pour l'index
+        '/guide_iceland_fr/index.html',
+        '/guide_iceland_fr/pages/main.html',
+        '/guide_iceland_fr/pages/installicon.html',
+      
+        '/guide_iceland_fr/images/logo.png'
+    ];
 ];
 
 // Installation du Service Worker
