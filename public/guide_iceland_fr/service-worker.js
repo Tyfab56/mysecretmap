92c1e062fa4d884aa86c4d19507d20d72d18ff5b
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-09-29 11:24:55 +0400

    guide

---
+++ public/guide_iceland_fr/service-worker.js
@@ -0,0 +1,54 @@
+// service-worker.js
+
+// Nom du cache
+const CACHE_NAME = "v1";
+
+// Liste des fichiers à mettre en cache
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/pages/main.html",
+    "/css/style.css",
+    "/js/app.js",
+    "/images/maplogo.gif",
+];
+
+// Installation du Service Worker
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Interception des requêtes et récupération des fichiers en cache
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Si la ressource est dans le cache, on la renvoie
+            if (response) {
+                return response;
+            }
+            // Sinon, on la récupère depuis le réseau
+            return fetch(event.request);
+        })
+    );
+});
+
+// Mise à jour du Service Worker
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
