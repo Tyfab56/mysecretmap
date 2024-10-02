391c73917f87813ca937832a9bd7efcc48fe8c7d
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-10-02 15:37:29 +0400

    guide

---
+++ public/guide_iceland_fr/js/routes.js
@@ -0,0 +1,13 @@
+// js/routes.js
+export default [
+    {
+        name: "main",
+        path: "/main/",
+        url: "./pages/main.html",
+    },
+    {
+        name: "installicon",
+        path: "/installicon/",
+        url: "./pages/installicon.html",
+    },
+];
