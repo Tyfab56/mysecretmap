f921b35e565d22eee7deb2d27d6928831116c133
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-10-23 07:38:16 +0400

    uploadguide

--- resources/views/frontend/addimagespot.blade.php
+++ resources/views/frontend/addimagespot.blade.php
@@ -16,195 +16,194 @@
 
 // Initialisation de Dropzone
 Dropzone.options.myDropzone = {
-    paramName: "file",
-    maxFilesize: 20, // Taille maximale des fichiers en Mo
-    acceptedFiles: ".jpeg,.jpg,.png,.gif", // Extensions autorisées
-    dictDefaultMessage: "Glissez-déposez vos fichiers ici ou cliquez pour sélectionner",
-    dictFallbackMessage: "Votre navigateur ne supporte pas le glisser-déposer de fichiers.",
-    dictFileTooBig: "Le fichier est trop volumineux. Taille maximale autorisée : 20Mo.",
-    dictInvalidFileType: "Vous ne pouvez pas téléverser des fichiers de ce type.",
-    dictResponseError: "Le serveur a répondu avec une erreur.",
-    dictCancelUpload: "Annuler l'envoi",
-    dictCancelUploadConfirmation: "Êtes-vous sûr de vouloir annuler cet envoi ?",
-    dictRemoveFile: "Supprimer le fichier",
-    dictRemoveFileConfirmation: null,
-    init: function() {
-      
-            this.on("totaluploadprogress", function(progress) {
-                document.querySelector("#progress .progress-bar").style.width = progress + "%";
-            });
-            this.on("queuecomplete", function() {
-                document.querySelector("#progress .progress-bar").style.width = "0%";
-            });
-    }
+paramName: "file",
+maxFilesize: 20, // Taille maximale des fichiers en Mo
+acceptedFiles: ".jpeg,.jpg,.png,.gif,.webp", // Extensions autorisées
+dictDefaultMessage: "Glissez-déposez vos fichiers ici ou cliquez pour sélectionner",
+dictFallbackMessage: "Votre navigateur ne supporte pas le glisser-déposer de fichiers.",
+dictFileTooBig: "Le fichier est trop volumineux. Taille maximale autorisée : 20Mo.",
+dictInvalidFileType: "Vous ne pouvez pas téléverser des fichiers de ce type.",
+dictResponseError: "Le serveur a répondu avec une erreur.",
+dictCancelUpload: "Annuler l'envoi",
+dictCancelUploadConfirmation: "Êtes-vous sûr de vouloir annuler cet envoi ?",
+dictRemoveFile: "Supprimer le fichier",
+dictRemoveFileConfirmation: null,
+init: function() {
+
+this.on("totaluploadprogress", function(progress) {
+document.querySelector("#progress .progress-bar").style.width = progress + "%";
+});
+this.on("queuecomplete", function() {
+document.querySelector("#progress .progress-bar").style.width = "0%";
+});
+}
 };
 
 function deletePicture (id)
-            {
-            let url = '{{route('delimagespot', ['Id'])}}';
-            url = url.replace('Id', id);
+{
+let url = '{{route('delimagespot', ['Id'])}}';
+url = url.replace('Id', id);
 
-            Swal.fire({
-            title: 'Confirmer la suppression?',
-            text: "Suppression definitive",
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Oui, supprimer'
-            }).then((result) => {
-            if (result.value) {
-            window.location.href = url;
-            }
-            });
-            }
-  
-          
+Swal.fire({
+title: 'Confirmer la suppression?',
+text: "Suppression definitive",
+type: 'warning',
+showCancelButton: true,
+confirmButtonColor: '#3085d6',
+cancelButtonColor: '#d33',
+confirmButtonText: 'Oui, supprimer'
+}).then((result) => {
+if (result.value) {
+window.location.href = url;
+}
+});
+}
+
+
 @endsection
 @section('content')
 @auth
 @if (auth()->user()->isPhotographer())
 <section id="ts-features" class="ts-features">
     <div class="container">
-      <div class="row">
-                
-      </div>
         <div class="row">
-          <div class="col-lg-12"><img  class="w100" src="{{$spot->imgpanolarge??''}}"></div>
+
+        </div>
+        <div class="row">
+            <div class="col-lg-12"><img class="w100" src="{{$spot->imgpanolarge??''}}"></div>
         </div>
         <div class="col-12">
-        <form action="{{ route('addimagespot.storedz') }}" class="dropzone" id="myDropzone">
-              @csrf
-              <input type="hidden" id="spotid" name="spotid" value="{{$spot->id}}">
-          </form>
+            <form action="{{ route('addimagespot.storedz') }}" class="dropzone" id="myDropzone">
+                @csrf
+                <input type="hidden" id="spotid" name="spotid" value="{{$spot->id}}">
+            </form>
 
-          <div id="progress" class="progress">
-              <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
+            <div id="progress" class="progress">
+                <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
 
-          <a class="btn btn-secondary mb-5" href="/destination/{{ $spot->pays_id }}/{{ $spot->id }}">RETOUR PAGE PRECEDENTE</a>
-          
-      
+            <a class="btn btn-secondary mb-5" href="/destination/{{ $spot->pays_id }}/{{ $spot->id }}">RETOUR PAGE PRECEDENTE</a>
+
+
         </div>
-            <div class="row">
-                <div class="col-lg-2 bgregbox">
+        <div class="row">
+            <div class="col-lg-2 bgregbox">
                 <p><b>Nombre d'images :</b> {{ $spottotalcount }}</p>
-                </div>
-                <div class="col-lg-10">
+            </div>
+            <div class="col-lg-10">
 
-        
-                <div class="gallery-container" > 
+
+                <div class="gallery-container">
                     <h2 class="heading-text">Responsive <span>image gallery</span></h2>
-                    <!-- header text --> 
-                    
-                    <ul class = "image-gallery" > 
-                    @foreach($pictures as $photo)
+                    <!-- header text -->
 
-                    <<li>
-                        <img src="{{ $photo->medium }}" class="mainimg" alt="Image Title" /> 
-                        <div class="overlay">
-                            <span>Titre de l'image</span>
-                            <a href="{{ route('delimagespot', $photo->id) }}" class="delete-photo" onclick="return confirm('Are you sure you want to delete this image?');">
-                                <i class="fas fa-trash"></i>
-                            </a>
-                        </div>
-                    </li>
+                    <ul class="image-gallery">
+                        @foreach($pictures as $photo)
 
-                    
-                    @endforeach
-                    </ul> 
+                        <<li>
+                            <img src="{{ $photo->medium }}" class="mainimg" alt="Image Title" />
+                            <div class="overlay">
+                                <span>Titre de l'image</span>
+                                <a href="{{ route('delimagespot', $photo->id) }}" class="delete-photo" onclick="return confirm('Are you sure you want to delete this image?');">
+                                    <i class="fas fa-trash"></i>
+                                </a>
+                            </div>
+                            </li>
+
+
+                            @endforeach
+                    </ul>
                 </div>
-            
-            
 
-            
+
+
+
             </div>
         </div>
 
- @else
- Uniquement pour les photographes
- @endif
-@endauth
-<style>
-.picture-wrapper {
-    position: relative;
-}
+        @else
+        Uniquement pour les photographes
+        @endif
+        @endauth
+        <style>
+            .picture-wrapper {
+                position: relative;
+            }
 
-.delete-icon {
-    position: absolute;
-    top: 0;
-    right: 0;
-    cursor: pointer;
-}
+            .delete-icon {
+                position: absolute;
+                top: 0;
+                right: 0;
+                cursor: pointer;
+            }
 
-.gallery-container {
-  padding: 40px 5%;
-}
+            .gallery-container {
+                padding: 40px 5%;
+            }
 
-.heading-text {
-  margin-bottom: 2rem;
-  font-size: 2rem;
-}
+            .heading-text {
+                margin-bottom: 2rem;
+                font-size: 2rem;
+            }
 
-.heading-text span {
-  font-weight: 100;
-}
+            .heading-text span {
+                font-weight: 100;
+            }
 
-ul {
-  list-style: none;
-}
+            ul {
+                list-style: none;
+            }
 
-/* Responsive image gallery rules begin*/
+            /* Responsive image gallery rules begin*/
 
-.image-gallery {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
-}
+            .image-gallery {
+                display: flex;
+                flex-wrap: wrap;
+                gap: 10px;
+            }
 
-.image-gallery > li {
-  flex: 1 1 auto; /* or flex: auto; */
-  height: 300px;
-  cursor: pointer;
-  position: relative;
-}
+            .image-gallery>li {
+                flex: 1 1 auto;
+                /* or flex: auto; */
+                height: 300px;
+                cursor: pointer;
+                position: relative;
+            }
 
-.image-gallery::after {
-  content: "";
-  flex-grow: 999;
-}
+            .image-gallery::after {
+                content: "";
+                flex-grow: 999;
+            }
 
-.image-gallery li .mainimg {
-  object-fit: cover;
-  width: 100%;
-  height: 100%;
-  vertical-align: middle;
-  border-radius: 5px;
-}
+            .image-gallery li .mainimg {
+                object-fit: cover;
+                width: 100%;
+                height: 100%;
+                vertical-align: middle;
+                border-radius: 5px;
+            }
 
-.overlay {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: rgba(57, 57, 57, 0.502);
-  top: 0;
-  left: 0;
-  transform: scale(0);
-  transition: all 0.2s 0.1s ease-in-out;
-  color: #fff;
-  border-radius: 5px;
-  /* center overlay content */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+            .overlay {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                background: rgba(57, 57, 57, 0.502);
+                top: 0;
+                left: 0;
+                transform: scale(0);
+                transition: all 0.2s 0.1s ease-in-out;
+                color: #fff;
+                border-radius: 5px;
+                /* center overlay content */
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
 
-/* hover */
-.image-gallery li:hover .overlay {
-  transform: scale(1);
-}
+            /* hover */
+            .image-gallery li:hover .overlay {
+                transform: scale(1);
+            }
+        </style>
 
-
-</style>
-
-@endsection
+        @endsection
--- resources/views/frontend/guide_iceland_en.blade.php
+++ resources/views/frontend/guide_iceland_en.blade.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{Lang::locale()}}">
+
 <head>
-	 <meta charset="utf-8">
-            <meta name="csrf-token" content="{{ csrf_token() }}">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-            <meta name="description" content="My Secret Map, the best place to discover new touristic spots for your next trip">
-            <meta name="author" content="">
-            <meta name="keywords" content="spot,tourisme,photo, voyage">
-            <meta name="robots" content="all">
-            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="description" content="My Secret Map, the best place to discover new touristic spots for your next trip">
+    <meta name="author" content="">
+    <meta name="keywords" content="spot,tourisme,photo, voyage">
+    <meta name="robots" content="all">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 </head>
+
 <body>
-        <div class="body-inner">
-			<div class="col-lg-4 center">
-				 <deckgo-demo 
-						 src="https://mysecretmap.com/audioguide_iceland_en"
-						 instant="true"
-						 style="width: 50vw; height: 100vh;">
+    <div class="body-inner">
+        <div class="col-lg-4 center">
+            <deckgo-demo
+                src="https://mysecretmap.com/audioguide_iceland_en"
+                instant="true"
+                style="width: 50vw; height: 100vh;">
 
-				</deckgo-demo>
-			</div>	
-	    </div>
+            </deckgo-demo>
+        </div>
+    </div>
 
-<script type="module" src="https://unpkg.com/@deckdeckgo/demo@latest/dist/deckdeckgo-demo/deckdeckgo-demo.esm.js"></script>
-<script nomodule="" src="https://unpkg.com/@deckdeckgo/demo@latest/dist/deckdeckgo-demo/deckdeckgo-demo.js"></script>	
+    <script type="module" src="https://unpkg.com/@deckdeckgo/demo@latest/dist/deckdeckgo-demo/deckdeckgo-demo.esm.js"></script>
+    <script nomodule="" src="https://unpkg.com/@deckdeckgo/demo@latest/dist/deckdeckgo-demo/deckdeckgo-demo.js"></script>
 </body>
+
 </html>
