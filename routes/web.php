09d7fbd224767d3fd3ade266caaaa48f9514423b
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-10-24 16:00:27 +0400

    uploadguide

--- routes/web.php
+++ routes/web.php
@@ -340,6 +340,6 @@ Route::post('/admin/import-audioguides', [AudioguideController::class, 'importAu
 // Add this route in routes/web.php
 // Ne pas laisser Laravel gérer les routes de la PWA
 Route::get('/audioguide_iceland/{any}', function () {
-    return view('index'); // Your PWA entry point
+    return File::get(public_path() . '/audioguide_iceland/index.html');
 })->where('any', '.*');
 require __DIR__ . '/auth.php';
