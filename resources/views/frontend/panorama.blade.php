736d057180f41fc0470f4f45f0cbbff392e69438
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-12-10 20:28:18 +0400

    gift

--- resources/views/frontend/panorama.blade.php
+++ resources/views/frontend/panorama.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panorama - {{ $panoramaData['name'] }}</title>
+
     <script src="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css" rel="stylesheet" />
 </head>
