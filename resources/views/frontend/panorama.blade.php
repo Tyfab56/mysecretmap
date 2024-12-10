2e2a0316c10954660cacde58303e15fe86d36f63
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-12-10 20:34:20 +0400

    gift

--- resources/views/frontend/panorama.blade.php
+++ resources/views/frontend/panorama.blade.php
@@ -7,10 +7,27 @@
 
     <script src="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css" rel="stylesheet" />
+
+    <style>
+        html,
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+            /* Empêche les scrollbars */
+        }
+
+        #panorama {
+            width: 100%;
+            height: 100%;
+        }
+    </style>
 </head>
 
 <body>
-    <div id="panorama" style="width: 100%; height: 100vh;"></div>
+    <div id="panorama"></div>
     <script>
         pannellum.viewer('panorama', {
             type: 'equirectangular',
