8afcf3b00328eecc40f101f32713672690ab713c
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2025-02-06 14:50:09 +0400

    newhome

--- resources/views/frontend2/index.blade.php
+++ resources/views/frontend2/index.blade.php
@@ -1,8 +1,12 @@
-<div class="container">
-    <div class="section-big-slider--title main-title">
-        <h2>Nous ne vendons pas des guides<br><strong>nous vendons le frisson de l’exploration
-                sans se
-                perdre</strong></h2>
-    </div>
+@extends('frontend.main_master')
 
-</div>
+@section('content')
+    <div class="container">
+        <div class="section-big-slider--title main-title">
+            <h2>Nous ne vendons pas des guides<br><strong>nous vendons le frisson de l’exploration
+                    sans se
+                    perdre</strong></h2>
+        </div>
+
+    </div>
+@endsection
